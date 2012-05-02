3561fd2f4309296ecb1fb6831aa95d819a825865
Debarshi Ray <debarshir@gnome.org>
2012-05-02 17:14:41 +0200

    Add PhotosSettings

---
+++ src/photos-settings.h
@@ -0,0 +1,71 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2012 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef PHOTOS_SETTINGS_H
+#define PHOTOS_SETTINGS_H
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#define PHOTOS_TYPE_SETTINGS (photos_settings_get_type ())
+
+#define PHOTOS_SETTINGS(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+   PHOTOS_TYPE_SETTINGS, PhotosSettings))
+
+#define PHOTOS_SETTINGS_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), \
+   PHOTOS_TYPE_SETTINGS, PhotosSettingsClass))
+
+#define PHOTOS_IS_SETTINGS(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+   PHOTOS_TYPE_SETTINGS))
+
+#define PHOTOS_IS_SETTINGS_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+   PHOTOS_TYPE_SETTINGS))
+
+#define PHOTOS_SETTINGS_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+   PHOTOS_TYPE_SETTINGS, PhotosSettingsClass))
+
+typedef struct _PhotosSettings        PhotosSettings;
+typedef struct _PhotosSettingsClass   PhotosSettingsClass;
+typedef struct _PhotosSettingsPrivate PhotosSettingsPrivate;
+
+struct _PhotosSettings
+{
+  GSettings parent_instance;
+  PhotosSettingsPrivate *priv;
+};
+
+struct _PhotosSettingsClass
+{
+  GSettingsClass parent_class;
+};
+
+GType                     photos_settings_get_type           (void) G_GNUC_CONST;
+
+GSettings                *photos_settings_new                (void);
+
+G_END_DECLS
+
+#endif /* PHOTOS_SETTINGS_H */
