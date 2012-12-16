424d79f433c2411cab72783fe70e090548c4415d
Debarshi Ray <debarshir@gnome.org>
2012-12-17 01:54:19 +0530

    source: Drop the gp:goa-account: prefix from the ID
    The prefix is redundant. The goa-account part is unnecessary because
    the fact that accounts come from GOA is implied. The gp part is
    unnecessary because there is no need for application specific
    prefixes. In the end, it is part of GNOME and not a particular
    application.
    
    This simplifies addition and removal of GoaObjects in
    PhotosSourceManager.

--- src/photos-source.c
+++ src/photos-source.c
@@ -155,7 +155,6 @@ photos_source_set_property (GObject *object, guint prop_id, const GValue *value,
       {
         GoaAccount *account;
         GoaObject *object;
-        const gchar *id;
         const gchar *provider_icon;
         const gchar *provider_name;
 
@@ -164,9 +163,7 @@ photos_source_set_property (GObject *object, guint prop_id, const GValue *value,
           break;
 
         account = goa_object_peek_account (object);
-
-        id = goa_account_get_id (account);
-        priv->id = g_strdup_printf ("gp:goa-account:%s", id);
+        priv->id = goa_account_dup_id (account);
 
         provider_icon = goa_account_get_provider_icon (account);
         priv->icon = g_icon_new_for_string (provider_icon, NULL); /* TODO: use a GError */
