GNOME/gnome-photos

commit 37dfb5cc0569b979568cc688cbd2ccf0efd357bd

Switch everything to G_ADD_PRIVATE()!
Since after commit f0b9f17bd we now depend on new enough GTK+/GLib
versions to be able to use GtkWidget templates, let's switch to
G_ADD_PRIVATE()/G_DEFINE_TYPE_WITH_PRIVATE() and friends for all the
photos-* objects.

https://bugzilla.gnome.org/show_bug.cgi?id=705551

src/photos-remote-display-manager.c
@@ -50,7 +50,7 @@ static guint signals[LAST_SIGNAL] = { 0 };
 
 static GObject *remote_display_manager_singleton = NULL;
 
-G_DEFINE_TYPE (PhotosRemoteDisplayManager, photos_remote_display_manager, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosRemoteDisplayManager, photos_remote_display_manager, G_TYPE_OBJECT);
 
 
 static Share *
@@ -123,8 +123,7 @@ photos_remote_display_manager_init (PhotosRemoteDisplayManager *self)
 {
   PhotosRemoteDisplayManagerPrivate *priv;
 
-  self->priv = priv = G_TYPE_INSTANCE_GET_PRIVATE (self, PHOTOS_TYPE_REMOTE_DISPLAY_MANAGER,
-                                            PhotosRemoteDisplayManagerPrivate);
+  self->priv = priv = photos_remote_display_manager_get_instance_private (self);
 
   /* Keep a connection to the renderers manager alive to keep the list of
    * renderers up-to-date */
@@ -160,8 +159,6 @@ photos_remote_display_manager_class_init (PhotosRemoteDisplayManagerClass *class
                                        PHOTOS_TYPE_DLNA_RENDERER,
                                        PHOTOS_TYPE_BASE_ITEM,
                                        G_TYPE_ERROR);
-
-  g_type_class_add_private (class, sizeof (PhotosRemoteDisplayManagerPrivate));
 }
 
 
@@ -176,18 +173,18 @@ photos_remote_display_manager_share_cb (GObject      *source_object,
   GError *error = NULL;
 
   item = photos_dlna_renderer_share_finish (renderer, res, &error);
+  g_object_unref (item); /* We already hold a ref to the item to be shared */
 
   if (error != NULL)
     {
       g_warning ("Unable to remotely display item '%s': %s",
-                 share->item != NULL ? photos_base_item_get_id (share->item) : "(none)",
+                 item != NULL ? photos_base_item_get_id (item) : "(none)",
                  error->message);
       g_signal_emit (share->manager, signals[SHARE_ERROR], 0, share->renderer, share->item, error);
       g_error_free (error);
       goto out;
     }
 
-  g_object_unref (item); /* We already hold a ref to the item to be shared */
   g_signal_emit (share->manager, signals[SHARE_BEGAN], 0, share->renderer, share->item);
 
 out:

src/photos-application.c
@@ -66,7 +66,7 @@ struct _PhotosApplicationPrivate
 };
 
 
-G_DEFINE_TYPE (PhotosApplication, photos_application, GTK_TYPE_APPLICATION)
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosApplication, photos_application, GTK_TYPE_APPLICATION)
 
 
 enum
@@ -558,9 +558,7 @@ photos_application_dispose (GObject *object)
 static void
 photos_application_init (PhotosApplication *self)
 {
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
-                                            PHOTOS_TYPE_APPLICATION,
-                                            PhotosApplicationPrivate);
+  self->priv = photos_application_get_instance_private (self);
   eog_debug_init ();
 }
 
@@ -577,8 +575,6 @@ photos_application_class_init (PhotosApplicationClass *class)
   application_class->startup = photos_application_startup;
 
   /* TODO: Add miners-changed signal */
-
-  g_type_class_add_private (class, sizeof (PhotosApplicationPrivate));
 }
 
 

src/photos-base-item.c
@@ -88,7 +88,7 @@ enum
 static guint signals[LAST_SIGNAL] = { 0 };
 
 
-G_DEFINE_TYPE (PhotosBaseItem, photos_base_item, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosBaseItem, photos_base_item, G_TYPE_OBJECT);
 
 
 typedef struct _PhotosBaseItemAsyncStrData PhotosBaseItemAsyncStrData;
@@ -866,7 +866,7 @@ photos_base_item_init (PhotosBaseItem *self)
 {
   PhotosBaseItemPrivate *priv;
 
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, PHOTOS_TYPE_BASE_ITEM, PhotosBaseItemPrivate);
+  self->priv = photos_base_item_get_instance_private (self);
   priv = self->priv;
 
   g_mutex_init (&priv->mutex_create_thumbnail);
@@ -923,8 +923,6 @@ photos_base_item_class_init (PhotosBaseItemClass *class)
                                         g_cclosure_marshal_VOID__VOID,
                                         G_TYPE_NONE,
                                         0);
-
-  g_type_class_add_private (class, sizeof (PhotosBaseItemPrivate));
 }
 
 

src/photos-base-manager.c
@@ -49,7 +49,7 @@ enum
 static guint signals[LAST_SIGNAL] = { 0 };
 
 
-G_DEFINE_TYPE (PhotosBaseManager, photos_base_manager, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosBaseManager, photos_base_manager, G_TYPE_OBJECT);
 
 
 static gboolean
@@ -134,7 +134,7 @@ photos_base_manager_init (PhotosBaseManager *self)
 {
   PhotosBaseManagerPrivate *priv;
 
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, PHOTOS_TYPE_BASE_MANAGER, PhotosBaseManagerPrivate);
+  self->priv = photos_base_manager_get_instance_private (self);
   priv = self->priv;
 
   priv->objects = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_object_unref);
@@ -195,8 +195,6 @@ photos_base_manager_class_init (PhotosBaseManagerClass *class)
                                           G_TYPE_NONE,
                                           1,
                                           G_TYPE_OBJECT);
-
-  g_type_class_add_private (class, sizeof (PhotosBaseManagerPrivate));
 }
 
 

src/photos-collection-icon-watcher.c
@@ -64,7 +64,7 @@ enum
 static guint signals[LAST_SIGNAL] = { 0 };
 
 
-G_DEFINE_TYPE (PhotosCollectionIconWatcher, photos_collection_icon_watcher, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosCollectionIconWatcher, photos_collection_icon_watcher, G_TYPE_OBJECT);
 
 
 static void
@@ -383,9 +383,7 @@ photos_collection_icon_watcher_init (PhotosCollectionIconWatcher *self)
 {
   PhotosCollectionIconWatcherPrivate *priv = self->priv;
 
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
-                                            PHOTOS_TYPE_COLLECTION_ICON_WATCHER,
-                                            PhotosCollectionIconWatcherPrivate);
+  self->priv = photos_collection_icon_watcher_get_instance_private (self);
   priv = self->priv;
 
   priv->item_connections = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, g_object_unref);
@@ -423,8 +421,6 @@ photos_collection_icon_watcher_class_init (PhotosCollectionIconWatcherClass *cla
                                         G_TYPE_NONE,
                                         1,
                                         G_TYPE_ICON);
-
-  g_type_class_add_private (class, sizeof (PhotosCollectionIconWatcherPrivate));
 }
 
 

src/photos-create-collection-job.c
@@ -50,7 +50,7 @@ enum
 };
 
 
-G_DEFINE_TYPE (PhotosCreateCollectionJob, photos_create_collection_job, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosCreateCollectionJob, photos_create_collection_job, G_TYPE_OBJECT);
 
 
 static void
@@ -154,9 +154,7 @@ photos_create_collection_job_init (PhotosCreateCollectionJob *self)
 {
   PhotosCreateCollectionJobPrivate *priv;
 
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
-                                            PHOTOS_TYPE_CREATE_COLLECTION_JOB,
-                                            PhotosCreateCollectionJobPrivate);
+  self->priv = photos_create_collection_job_get_instance_private (self);
   priv = self->priv;
 
   priv->queue = photos_tracker_queue_new ();
@@ -179,8 +177,6 @@ photos_create_collection_job_class_init (PhotosCreateCollectionJobClass *class)
                                                         "The name of the collection to be created",
                                                         NULL,
                                                         G_PARAM_CONSTRUCT_ONLY | G_PARAM_WRITABLE));
-
-  g_type_class_add_private (class, sizeof (PhotosCreateCollectionJobPrivate));
 }
 
 

src/photos-delete-item-job.c
@@ -49,7 +49,7 @@ enum
 };
 
 
-G_DEFINE_TYPE (PhotosDeleteItemJob, photos_delete_item_job, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (PhotosDeleteItemJob, photos_delete_item_job, G_TYPE_OBJECT);
 
 
 static void
@@ -120,9 +120,7 @@ photos_delete_item_job_init (PhotosDeleteItemJob *self)
 {
   PhotosDeleteItemJobPrivate *priv;
 
-  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
-                                            PHOTOS_TYPE_DELETE_ITEM_JOB,
-                                            PhotosDeleteItemJobPrivate);
+  self->priv = photos_delete_item_job_get_instance_private (self);
   priv = self->priv;
 
   priv->queue = photos_tracker_queue_new ();
@@ -145,8 +143,6 @@ photos_delete_item_job_class_init (PhotosDeleteItemJobClass *class)
                                                         "An unique ID associated with this item",
                                                         NULL,
                                                         G_PARAM_CONSTRUCT_ONLY | G_PARAM_WRITABLE));
-
-  g_type_class_add_private (class, sizeof (PhotosDeleteItemJobPrivate));
 }
 
 

src/photos-dlna-renderer.c
@@ -56,6 +56,7 @@ static void photos_dlna_renderer_async_initable_iface_init (GAsyncInitableIface 
 
 
 G_DEFINE_TYPE_WITH_CODE (PhotosDlnaRenderer, photos_dlna_renderer, G_TYPE_OBJECT,
+                         G_ADD_PRIVATE (PhotosDlnaRenderer)
                          G_IMPLEMENT_INTERFACE (G_TYPE_ASYNC_INITABLE, photos_dlna_renderer_async_initable_iface_init));
 
 #define RETURN_ON_ERROR(task, error, msg) \
@@ -157,8 +158,7 @@ photos_dlna_renderer_init (PhotosDlnaRenderer *self)
 {
   PhotosDlnaRendererPrivate *priv;
 
-  self->priv = priv = G_TYPE_INSTANCE_GET_PRIVATE (self, PHOTOS_TYPE_DLNA_RENDERER,
-                                                   PhotosDlnaRendererPrivate);
+  self->priv = priv = photos_dlna_renderer_get_instance_private (self);
 
   priv->urls_to_item = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_object_unref);
 }
@@ -236,8 +236,6 @@ photos_dlna_renderer_class_init (PhotosDlnaRendererClass *class)
                                                       G_PARAM_STATIC_NAME |
                                                       G_PARAM_STATIC_BLURB |
                                                       G_PARAM_STATIC_NICK));
-
-  g_type_class_add_private (class, sizeof (PhotosDlnaRendererPrivate));
 }
 
 

commit 14ebdacdb33103f932107746d2c2068d80d7bc90

remote-display-manager: Avoid unref'ing NULL in case of errors, again
Ouch. Commit 37dfb5cc inadvertently reverted 3f01ccd, reapply.

https://bugzilla.gnome.org/show_bug.cgi?id=705435

src/photos-remote-display-manager.c
@@ -173,18 +173,18 @@ photos_remote_display_manager_share_cb (GObject      *source_object,
   GError *error = NULL;
 
   item = photos_dlna_renderer_share_finish (renderer, res, &error);
-  g_object_unref (item); /* We already hold a ref to the item to be shared */
 
   if (error != NULL)
     {
       g_warning ("Unable to remotely display item '%s': %s",
-                 item != NULL ? photos_base_item_get_id (item) : "(none)",
+                 share->item != NULL ? photos_base_item_get_id (share->item) : "(none)",
                  error->message);
       g_signal_emit (share->manager, signals[SHARE_ERROR], 0, share->renderer, share->item, error);
       g_error_free (error);
       goto out;
     }
 
+  g_object_unref (item); /* We already hold a ref to the item to be shared */
   g_signal_emit (share->manager, signals[SHARE_BEGAN], 0, share->renderer, share->item);
 
 out: