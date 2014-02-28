GNOME/gnome-photos

commit 5e6a86718aabf7731f885adcaf5b7ef4da905d3b

application: Implement activate_result

src/photos-application.c
@@ -43,10 +43,12 @@
 #include "photos-main-window.h"
 #include "photos-mode-controller.h"
 #include "photos-properties-dialog.h"
+#include "photos-query.h"
 #include "photos-resources.h"
 #include "photos-search-context.h"
 #include "photos-search-controller.h"
 #include "photos-search-provider.h"
+#include "photos-single-item-job.h"
 #include "photos-source-manager.h"
 
 
@@ -178,11 +180,69 @@ photos_application_create_window (PhotosApplication *self)
 
 
 static void
+photos_application_activate_item (PhotosApplication *self, GObject *item)
+{
+  PhotosApplicationPrivate *priv = self->priv;
+
+  photos_base_manager_set_active_object (priv->item_mngr, item);
+
+  photos_application_create_window (self);
+  photos_mode_controller_set_window_mode (priv->mode_cntrlr, PHOTOS_WINDOW_MODE_PREVIEW);
+  g_application_activate (G_APPLICATION (self));
+
+  /* TODO: Forward the search terms when we exit the preview */
+}
+
+
+static void
+photos_application_activate_query_executed (TrackerSparqlCursor *cursor, gpointer user_data)
+{
+  PhotosApplication *self = PHOTOS_APPLICATION (user_data);
+  PhotosApplicationPrivate *priv = self->priv;
+  GObject *item;
+  const gchar *identifier;
+
+  if (cursor == NULL)
+    goto out;
+
+  photos_item_manager_add_item (PHOTOS_ITEM_MANAGER (priv->item_mngr), cursor);
+
+  identifier = tracker_sparql_cursor_get_string (cursor, PHOTOS_QUERY_COLUMNS_URN, NULL);
+  item = photos_base_manager_get_object_by_id (priv->item_mngr, identifier);
+
+  photos_application_activate_item (self, item);
+
+ out:
+  g_object_unref (self);
+}
+
+
+static void
 photos_application_activate_result (PhotosApplication *self,
                                     const gchar *identifier,
                                     const gchar *const *terms,
                                     guint timestamp)
 {
+  PhotosApplicationPrivate *priv = self->priv;
+  GObject *item;
+
+  priv->activation_timestamp = timestamp;
+
+  item = photos_base_manager_get_object_by_id (priv->item_mngr, identifier);
+  if (item != NULL)
+    photos_application_activate_item (self, item);
+  else
+    {
+      PhotosSingleItemJob *job;
+
+      job = photos_single_item_job_new (identifier);
+      photos_single_item_job_run (job,
+                                  priv->state,
+                                  PHOTOS_QUERY_FLAGS_UNFILTERED,
+                                  photos_application_activate_query_executed,
+                                  g_object_ref (self));
+      g_object_unref (job);
+    }
 }
 
 

commit 8f5109445fcd35b0d05f1015b022f0ea5990b41e

Unbreak activating a search provider result
I swear I had tested it before pushing, but looks I made a mistake.

We have to create the main window before setting the active item.
Otherwise the emission of PhotosItemManager::active-changed will be
missed. Secondly, since we are not moving from a view with an
associated model we should skip setting it.

Fallout from 5e6a86718aabf7731f885adcaf5b7ef4da905d3b

Fixes: https://bugzilla.gnome.org/725328

src/photos-application.c
@@ -184,10 +184,8 @@ photos_application_activate_item (PhotosApplication *self, GObject *item)
 {
   PhotosApplicationPrivate *priv = self->priv;
 
-  photos_base_manager_set_active_object (priv->item_mngr, item);
-
   photos_application_create_window (self);
-  photos_mode_controller_set_window_mode (priv->mode_cntrlr, PHOTOS_WINDOW_MODE_PREVIEW);
+  photos_base_manager_set_active_object (priv->item_mngr, item);
   g_application_activate (G_APPLICATION (self));
 
   /* TODO: Forward the search terms when we exit the preview */

src/photos-embed.c
@@ -164,9 +164,6 @@ photos_embed_get_view_container_from_mode (PhotosEmbed *self, PhotosWindowMode m
       view_container = priv->overview;
       break;
 
-    case PHOTOS_WINDOW_MODE_PREVIEW:
-      view_container = NULL;
-      break;
 
     case PHOTOS_WINDOW_MODE_SEARCH:
       view_container = priv->search;
@@ -228,7 +225,6 @@ photos_embed_item_load (GObject *source_object, GAsyncResult *res, gpointer user
   GeglNode *node;
   GtkListStore *model;
   GtkTreePath *current_path;
-  GtkWidget *view_container;
   PhotosBaseItem *item = PHOTOS_BASE_ITEM (source_object);
   PhotosWindowMode mode;
 
@@ -240,13 +236,19 @@ photos_embed_item_load (GObject *source_object, GAsyncResult *res, gpointer user
     goto out;
 
   mode = photos_mode_controller_get_window_mode (priv->mode_cntrlr);
-  view_container = photos_embed_get_view_container_from_mode (self, mode);
 
-  /* If we are already in the preview and navigating using the
-   * buttons, then we don't need this.
+  /* This is not needed when:
+   *  - activated from the search provider
+   *  - already in the preview and navigating using the buttons
    */
-  if (mode != PHOTOS_WINDOW_MODE_PREVIEW)
+  if (mode == PHOTOS_WINDOW_MODE_COLLECTIONS
+      || mode == PHOTOS_WINDOW_MODE_FAVORITES
+      || mode == PHOTOS_WINDOW_MODE_OVERVIEW
+      || mode == PHOTOS_WINDOW_MODE_SEARCH)
     {
+      GtkWidget *view_container;
+
+      view_container = photos_embed_get_view_container_from_mode (self, mode);
       current_path = photos_view_container_get_current_path (PHOTOS_VIEW_CONTAINER (view_container));
       model = photos_view_container_get_model (PHOTOS_VIEW_CONTAINER (view_container));
       photos_preview_view_set_model (PHOTOS_PREVIEW_VIEW (priv->preview), GTK_TREE_MODEL (model), current_path);