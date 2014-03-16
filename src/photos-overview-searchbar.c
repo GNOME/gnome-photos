GNOME/gnome-photos

commit 4742182024826482e77c4a50215496488a2090c9

overview-searchbar: Refocus the search entry after changing filters
Original patch from Rohit Agarwal for gnome-documents.

Fixes: https://bugzilla.gnome.org/726165

src/photos-overview-searchbar.c
@@ -69,6 +69,7 @@ photos_overview_searchbar_active_changed (PhotosOverviewSearchbar *self,
                                           GdTaggedEntryTag *tag)
 {
   PhotosOverviewSearchbarPrivate *priv = self->priv;
+  GdkDevice *event_device;
   GObject *object;
   const gchar *id;
   gchar *name;
@@ -84,6 +85,10 @@ photos_overview_searchbar_active_changed (PhotosOverviewSearchbar *self,
       gd_tagged_entry_tag_set_label (tag, name);
       gd_tagged_entry_add_tag (priv->search_entry, tag);
     }
+
+  event_device = gtk_get_current_event_device ();
+  if (event_device != NULL)
+    gd_entry_focus_hack (priv->search_entry, event_device);
 
   g_free (name);
 }

commit e05816bb82f7d8b73fb65c1adc475bd9c5f64280

overview-searchbar: Silence a compiler warning
Fallout from 4742182024826482e77c4a50215496488a2090c9

Fixes: https://bugzilla.gnome.org/726165

src/photos-overview-searchbar.c
@@ -88,7 +88,7 @@ photos_overview_searchbar_active_changed (PhotosOverviewSearchbar *self,
 
   event_device = gtk_get_current_event_device ();
   if (event_device != NULL)
-    gd_entry_focus_hack (priv->search_entry, event_device);
+    gd_entry_focus_hack (GTK_WIDGET (priv->search_entry), event_device);
 
   g_free (name);
 }