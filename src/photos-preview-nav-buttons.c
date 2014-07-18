GNOME/gnome-photos

commit edc439c92044b04395e88391d8379f4911eb65bc

Use arrow keys for navigating in preview mode
https://bugzilla.gnome.org/show_bug.cgi?id=726685

src/photos-preview-nav-buttons.c
@@ -306,22 +306,6 @@ photos_preview_nav_buttons_set_active_path (PhotosPreviewNavButtons *self)
 
 
 static void
-photos_preview_nav_buttons_next_clicked (PhotosPreviewNavButtons *self)
-{
-  gtk_tree_path_next (self->priv->current_path);
-  photos_preview_nav_buttons_set_active_path (self);
-}
-
-
-static void
-photos_preview_nav_buttons_prev_clicked (PhotosPreviewNavButtons *self)
-{
-  gtk_tree_path_prev (self->priv->current_path);
-  photos_preview_nav_buttons_set_active_path (self);
-}
-
-
-static void
 photos_preview_nav_buttons_dispose (GObject *object)
 {
   PhotosPreviewNavButtons *self = PHOTOS_PREVIEW_NAV_BUTTONS (object);
@@ -378,7 +362,7 @@ photos_preview_nav_buttons_constructed (GObject *object)
   gtk_container_add (GTK_CONTAINER (priv->prev_widget), button);
   g_signal_connect_swapped (button,
                             "clicked",
-                            G_CALLBACK (photos_preview_nav_buttons_prev_clicked),
+                            G_CALLBACK (photos_preview_nav_buttons_previous),
                             self);
   g_signal_connect_swapped (button,
                             "enter-notify-event",
@@ -407,7 +391,7 @@ photos_preview_nav_buttons_constructed (GObject *object)
   gtk_container_add (GTK_CONTAINER (priv->next_widget), button);
   g_signal_connect_swapped (button,
                             "clicked",
-                            G_CALLBACK (photos_preview_nav_buttons_next_clicked),
+                            G_CALLBACK (photos_preview_nav_buttons_next),
                             self);
   g_signal_connect_swapped (button,
                             "enter-notify-event",
@@ -536,6 +520,22 @@ photos_preview_nav_buttons_hide (PhotosPreviewNavButtons *self)
   photos_preview_nav_buttons_fade_out_button (self, priv->prev_widget);
   photos_preview_nav_buttons_fade_out_button (self, priv->next_widget);
   photos_preview_nav_buttons_fade_out_button (self, priv->toolbar_widget);
+}
+
+
+void
+photos_preview_nav_buttons_next (PhotosPreviewNavButtons *self)
+{
+  gtk_tree_path_next (self->priv->current_path);
+  photos_preview_nav_buttons_set_active_path (self);
+}
+
+
+void
+photos_preview_nav_buttons_previous (PhotosPreviewNavButtons *self)
+{
+  gtk_tree_path_prev (self->priv->current_path);
+  photos_preview_nav_buttons_set_active_path (self);
 }
 
 

src/photos-embed.c
@@ -38,7 +38,6 @@
 #include "photos-item-manager.h"
 #include "photos-mode-controller.h"
 #include "photos-notification-manager.h"
-#include "photos-preview-view.h"
 #include "photos-search-controller.h"
 #include "photos-selection-toolbar.h"
 #include "photos-spinner-box.h"
@@ -815,4 +814,11 @@ PhotosMainToolbar *
 photos_embed_get_main_toolbar (PhotosEmbed *self)
 {
   return PHOTOS_MAIN_TOOLBAR (self->priv->toolbar);
+}
+
+
+PhotosPreviewView *
+photos_embed_get_preview (PhotosEmbed *self)
+{
+  return PHOTOS_PREVIEW_VIEW (self->priv->preview);
 }

src/photos-embed.h
@@ -28,6 +28,7 @@
 #include <gtk/gtk.h>
 
 #include "photos-main-toolbar.h"
+#include "photos-preview-view.h"
 
 G_BEGIN_DECLS
 
@@ -73,6 +74,8 @@ GType                  photos_embed_get_type               (void) G_GNUC_CONST;
 GtkWidget             *photos_embed_new                    (void);
 
 PhotosMainToolbar     *photos_embed_get_main_toolbar       (PhotosEmbed *self);
+
+PhotosPreviewView     *photos_embed_get_preview            (PhotosEmbed *self);
 
 G_END_DECLS
 

src/photos-main-window.c
@@ -34,6 +34,7 @@
 #include "photos-item-manager.h"
 #include "photos-main-window.h"
 #include "photos-mode-controller.h"
+#include "photos-preview-view.h"
 #include "photos-search-context.h"
 #include "photos-selection-controller.h"
 #include "photos-settings.h"
@@ -229,19 +230,35 @@ static gboolean
 photos_main_window_handle_key_preview (PhotosMainWindow *self, GdkEventKey *event)
 {
   PhotosMainWindowPrivate *priv = self->priv;
+  PhotosPreviewView *preview;
   gboolean fullscreen;
+  gboolean handled = FALSE;
 
+  preview = photos_embed_get_preview (PHOTOS_EMBED (priv->embed));
   fullscreen = photos_mode_controller_get_fullscreen (priv->mode_cntrlr);
-  if (event->keyval == GDK_KEY_Escape)
+
+  switch (event->keyval)
     {
+    case GDK_KEY_Escape:
       if (fullscreen)
         {
           photos_base_manager_set_active_object (priv->item_mngr, NULL);
           photos_mode_controller_go_back (priv->mode_cntrlr);
         }
+      break;
+
+    case GDK_KEY_Left:
+      photos_preview_view_load_previous (preview);
+      handled = TRUE;
+      break;
+
+    case GDK_KEY_Right:
+      photos_preview_view_load_next (preview);
+      handled = TRUE;
+      break;
     }
 
-  return GDK_EVENT_PROPAGATE;
+  return handled;
 }
 
 

src/photos-preview-nav-buttons.h
@@ -76,6 +76,10 @@ PhotosPreviewNavButtons    *photos_preview_nav_buttons_new                (Photo
 
 void                        photos_preview_nav_buttons_hide               (PhotosPreviewNavButtons *self);
 
+void                        photos_preview_nav_buttons_next               (PhotosPreviewNavButtons *self);
+
+void                        photos_preview_nav_buttons_previous           (PhotosPreviewNavButtons *self);
+
 void                        photos_preview_nav_buttons_set_model          (PhotosPreviewNavButtons *self,
                                                                            GtkTreeModel *model,
                                                                            GtkTreePath *current_path);

src/photos-preview-view.c
@@ -243,6 +243,20 @@ photos_preview_view_new (GtkOverlay *overlay)
 
 
 void
+photos_preview_view_load_next (PhotosPreviewView *self)
+{
+  photos_preview_nav_buttons_next (self->priv->nav_buttons);
+}
+
+
+void
+photos_preview_view_load_previous (PhotosPreviewView *self)
+{
+  photos_preview_nav_buttons_previous (self->priv->nav_buttons);
+}
+
+
+void
 photos_preview_view_set_model (PhotosPreviewView *self, GtkTreeModel *model, GtkTreePath *current_path)
 {
   photos_preview_nav_buttons_set_model (self->priv->nav_buttons, model, current_path);

src/photos-preview-view.h
@@ -71,6 +71,10 @@ GType                  photos_preview_view_get_type               (void) G_GNUC_
 
 GtkWidget             *photos_preview_view_new                    (GtkOverlay *overlay);
 
+void                   photos_preview_view_load_next              (PhotosPreviewView *self);
+
+void                   photos_preview_view_load_previous          (PhotosPreviewView *self);
+
 void                   photos_preview_view_set_model              (PhotosPreviewView *self,
                                                                    GtkTreeModel *model,
                                                                    GtkTreePath *current_path);

commit e8a445ee37c3ff53b82b1eb557afdbebdb8ea6b1

preview-nav-buttons: Restrict keynav within the bounds of the model
We hide the buttons when after reaching one end of the model. We
should also disable keyboard navigation in such cases.

Fallout from edc439c92044b04395e88391d8379f4911eb65bc

Fixes: https://bugzilla.gnome.org/726685

src/photos-preview-nav-buttons.c
@@ -49,6 +49,8 @@ struct _PhotosPreviewNavButtonsPrivate
   GtkWidget *toolbar_widget;
   PhotosBaseManager *item_mngr;
   PhotosPreviewAction action;
+  gboolean enable_next;
+  gboolean enable_prev;
   gboolean hover;
   gboolean visible;
   guint auto_hide_id;
@@ -58,6 +60,8 @@ struct _PhotosPreviewNavButtonsPrivate
 enum
 {
   PROP_0,
+  PROP_ENABLE_NEXT,
+  PROP_ENABLE_PREVIOUS,
   PROP_OVERLAY,
   PROP_PREVIEW_VIEW
 };
@@ -237,34 +241,33 @@ photos_preview_nav_buttons_update_visibility (PhotosPreviewNavButtons *self)
   PhotosPreviewNavButtonsPrivate *priv = self->priv;
   GtkTreeIter iter;
   GtkTreeIter tmp;
+  gboolean enable_next = FALSE;
+  gboolean enable_prev = FALSE;
 
   if (priv->model == NULL
       || priv->current_path == NULL
       || !priv->visible
       || !gtk_tree_model_get_iter (priv->model, &iter, priv->current_path))
     {
-      photos_preview_nav_buttons_fade_out_button (self, priv->prev_widget);
-      photos_preview_nav_buttons_fade_out_button (self, priv->next_widget);
+      enable_prev = FALSE;
+      enable_next = FALSE;
       photos_preview_nav_buttons_fade_out_button (self, priv->toolbar_widget);
-      return;
+      goto out;
     }
 
   photos_preview_nav_buttons_fade_in_button (self, priv->toolbar_widget);
 
   tmp = iter;
-  if (gtk_tree_model_iter_previous (priv->model, &tmp))
-      photos_preview_nav_buttons_fade_in_button (self, priv->prev_widget);
-  else
-      photos_preview_nav_buttons_fade_out_button (self, priv->prev_widget);
+  enable_prev = gtk_tree_model_iter_previous (priv->model, &tmp);
 
   tmp = iter;
-  if (gtk_tree_model_iter_next (priv->model, &tmp))
-      photos_preview_nav_buttons_fade_in_button (self, priv->next_widget);
-  else
-      photos_preview_nav_buttons_fade_out_button (self, priv->next_widget);
+  enable_next = gtk_tree_model_iter_next (priv->model, &tmp);
 
   if (!priv->hover)
     photos_preview_nav_buttons_queue_auto_hide (self);
+
+ out:
+  g_object_set (self, "enable-next", enable_next, "enable-previous", enable_prev, NULL);
 }
 
 
@@ -290,6 +293,30 @@ photos_preview_nav_buttons_motion_notify (PhotosPreviewNavButtons *self)
                                      g_object_ref (self),
                                      g_object_unref);
   return FALSE;
+}
+
+
+static void
+photos_preview_nav_buttons_notify_enable_next (PhotosPreviewNavButtons *self)
+{
+  PhotosPreviewNavButtonsPrivate *priv = self->priv;
+
+  if (priv->enable_next)
+    photos_preview_nav_buttons_fade_in_button (self, priv->next_widget);
+  else
+    photos_preview_nav_buttons_fade_out_button (self, priv->next_widget);
+}
+
+
+static void
+photos_preview_nav_buttons_notify_enable_previous (PhotosPreviewNavButtons *self)
+{
+  PhotosPreviewNavButtonsPrivate *priv = self->priv;
+
+  if (priv->enable_prev)
+    photos_preview_nav_buttons_fade_in_button (self, priv->prev_widget);
+  else
+    photos_preview_nav_buttons_fade_out_button (self, priv->prev_widget);
 }
 
 
@@ -448,6 +475,29 @@ photos_preview_nav_buttons_constructed (GObject *object)
 
 
 static void
+photos_preview_nav_buttons_get_property (GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
+{
+  PhotosPreviewNavButtons *self = PHOTOS_PREVIEW_NAV_BUTTONS (object);
+  PhotosPreviewNavButtonsPrivate *priv = self->priv;
+
+  switch (prop_id)
+    {
+    case PROP_ENABLE_NEXT:
+      g_value_set_boolean (value, priv->enable_next);
+      break;
+
+    case PROP_ENABLE_PREVIOUS:
+      g_value_set_boolean (value, priv->enable_prev);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+
+static void
 photos_preview_nav_buttons_set_property (GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
 {
   PhotosPreviewNavButtons *self = PHOTOS_PREVIEW_NAV_BUTTONS (object);
@@ -455,6 +505,14 @@ photos_preview_nav_buttons_set_property (GObject *object, guint prop_id, const G
 
   switch (prop_id)
     {
+    case PROP_ENABLE_NEXT:
+      priv->enable_next = g_value_get_boolean (value);
+      break;
+
+    case PROP_ENABLE_PREVIOUS:
+      priv->enable_prev = g_value_get_boolean (value);
+      break;
+
     case PROP_OVERLAY:
       priv->overlay = GTK_WIDGET (g_value_dup_object (value));
       break;
@@ -485,6 +543,15 @@ photos_preview_nav_buttons_init (PhotosPreviewNavButtons *self)
                             self);
 
   priv->action = PHOTOS_PREVIEW_ACTION_NONE;
+
+  g_signal_connect (self,
+                    "notify::enable-next",
+                    G_CALLBACK (photos_preview_nav_buttons_notify_enable_next),
+                    NULL);
+  g_signal_connect (self,
+                    "notify::enable-previous",
+                    G_CALLBACK (photos_preview_nav_buttons_notify_enable_previous),
+                    NULL);
 }
 
 
@@ -496,7 +563,24 @@ photos_preview_nav_buttons_class_init (PhotosPreviewNavButtonsClass *class)
   object_class->constructed = photos_preview_nav_buttons_constructed;
   object_class->dispose = photos_preview_nav_buttons_dispose;
   object_class->finalize = photos_preview_nav_buttons_finalize;
+  object_class->get_property = photos_preview_nav_buttons_get_property;
   object_class->set_property = photos_preview_nav_buttons_set_property;
+
+  g_object_class_install_property (object_class,
+                                   PROP_ENABLE_NEXT,
+                                   g_param_spec_boolean ("enable-next",
+                                                         "Enable next",
+                                                         "Allow moving to the next item",
+                                                         FALSE,
+                                                         G_PARAM_READWRITE));
+
+  g_object_class_install_property (object_class,
+                                   PROP_ENABLE_PREVIOUS,
+                                   g_param_spec_boolean ("enable-previous",
+                                                         "Enable previous",
+                                                         "Allow moving to the previous item",
+                                                         FALSE,
+                                                         G_PARAM_READWRITE));
 
   g_object_class_install_property (object_class,
                                    PROP_OVERLAY,
@@ -561,6 +645,9 @@ photos_preview_nav_buttons_next (PhotosPreviewNavButtons *self)
 {
   PhotosPreviewNavButtonsPrivate *priv = self->priv;
 
+  if (!priv->enable_next)
+    return;
+
   priv->action = PHOTOS_PREVIEW_ACTION_NEXT;
   gtk_tree_path_next (priv->current_path);
   photos_preview_nav_buttons_set_active_path (self);
@@ -573,6 +660,9 @@ void
 photos_preview_nav_buttons_previous (PhotosPreviewNavButtons *self)
 {
   PhotosPreviewNavButtonsPrivate *priv = self->priv;
+
+  if (!priv->enable_prev)
+    return;
 
   priv->action = PHOTOS_PREVIEW_ACTION_PREVIOUS;
   gtk_tree_path_prev (priv->current_path);