GNOME/gnome-photos

commit 0c1f1e48ca17cb91d3720aec0fce4644bac61439

print-notification: Change return type of new to void
It is intended to be self managing and there is no need for the caller
to have a pointer to it.

src/photos-print-notification.c
@@ -198,7 +198,7 @@ photos_print_notification_class_init (PhotosPrintNotificationClass *class)
 }
 
 
-GtkWidget *
+void
 photos_print_notification_new (GtkPrintOperation *print_op)
 {
   return g_object_new (PHOTOS_TYPE_PRINT_NOTIFICATION,

src/photos-print-notification.h
@@ -68,7 +68,7 @@ struct _PhotosPrintNotificationClass
 
 GType               photos_print_notification_get_type           (void) G_GNUC_CONST;
 
-GtkWidget          *photos_print_notification_new                (GtkPrintOperation *print_op);
+void                photos_print_notification_new                (GtkPrintOperation *print_op);
 
 G_END_DECLS
 

commit b96b7eebf88e2322254036510805d483190fc9d9

print-notification: Silence a compiler warning
Fallout from 0c1f1e48ca17cb91d3720aec0fce4644bac61439

src/photos-print-notification.c
@@ -201,11 +201,11 @@ photos_print_notification_class_init (PhotosPrintNotificationClass *class)
 void
 photos_print_notification_new (GtkPrintOperation *print_op)
 {
-  return g_object_new (PHOTOS_TYPE_PRINT_NOTIFICATION,
-                       "column-spacing", 12,
-                       "margin-start", 12,
-                       "margin-end", 12,
-                       "orientation", GTK_ORIENTATION_HORIZONTAL,
-                       "print-op", print_op,
-                       NULL);
+  g_object_new (PHOTOS_TYPE_PRINT_NOTIFICATION,
+                "column-spacing", 12,
+                "margin-start", 12,
+                "margin-end", 12,
+                "orientation", GTK_ORIENTATION_HORIZONTAL,
+                "print-op", print_op,
+                NULL);
 }