d17d64f4c5630fd3291d96fd2074818fec838dee
Debarshi Ray <debarshir@gnome.org>
2015-12-22 17:42:56 +0100

    utils: Change PHOTOS_EXPORT_SUBPATH to be "Exports"
    https://bugzilla.gnome.org/show_bug.cgi?id=759363

--- src/photos-utils.h
+++ src/photos-utils.h
@@ -55,7 +55,7 @@ G_BEGIN_DECLS
 
 #define PHOTOS_COLLECTION_SCREENSHOT \
   "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#image-category-screenshot"
-#define PHOTOS_EXPORT_SUBPATH PACKAGE_TARNAME "-exported"
+#define PHOTOS_EXPORT_SUBPATH "Exports"
 
 void             photos_utils_border_pixbuf               (GdkPixbuf *pixbuf);
 
