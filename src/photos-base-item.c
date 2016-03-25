aeeacb39f18852794ff8d5826c991c2299f38180
Umang Jain <mailumangjain@gmail.com>
2016-03-25 13:02:28 +0530

    base-item: Don't leak the GFileInfo
    https://bugzilla.gnome.org/show_bug.cgi?id=763908

--- src/photos-base-item.c
+++ src/photos-base-item.c
@@ -741,7 +741,7 @@ photos_base_item_file_query_info (GObject *source_object, GAsyncResult *res, gpo
   PhotosBaseItemPrivate *priv = self->priv;
   GError *error = NULL;
   GFile *file = G_FILE (source_object);
-  GFileInfo *info;
+  GFileInfo *info = NULL;
 
   info = g_file_query_info_finish (file, res, &error);
   if (error != NULL)
@@ -765,6 +765,7 @@ photos_base_item_file_query_info (GObject *source_object, GAsyncResult *res, gpo
     }
 
  out:
+  g_clear_object (&info);
   g_object_unref (self);
 }
 
