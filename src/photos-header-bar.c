207ee2ba1b3f9460d074b582f2858605ab5903c0
Timm Bäder <mail@baedert.org>
2016-04-01 11:41:16 +0200

    header-bar: Plug memory leak

--- src/photos-header-bar.c
+++ src/photos-header-bar.c
@@ -113,6 +113,8 @@ photos_header_bar_clear (PhotosHeaderBar *self)
   children = gtk_container_get_children (GTK_CONTAINER (self));
   for (l = children; l != NULL; l = l->next)
     gtk_widget_destroy (GTK_WIDGET (l->data));
+
+  g_list_free (children);
 }
 
 
