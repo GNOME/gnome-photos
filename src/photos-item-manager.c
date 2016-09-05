GNOME/gnome-photos

commit 8d1d00232a9e62deb93ae4d4f498d0b51b3dad2e

item-manager: Add photos_item_manager_clear
Encouraging the TrackerControllers to take a peek at the child
BaseManagers doesn't sound like a good idea. Plus, it is slightly more
efficient to update the 0th child if the ItemManager is in control of
the operation - we can iterate over the, hopefully, smaller child
BaseManager instead of the union of all children.

https://bugzilla.gnome.org/show_bug.cgi?id=770342

src/photos-item-manager.c
@@ -779,6 +779,53 @@ photos_item_manager_add_item_for_mode (PhotosItemManager *self, PhotosWindowMode
 }
 
 
+void
+photos_item_manager_clear (PhotosItemManager *self, PhotosWindowMode mode)
+{
+  GHashTable *items;
+  GHashTableIter iter;
+  PhotosBaseManager *item_mngr_chld;
+  const gchar *id;
+
+  g_return_if_fail (PHOTOS_IS_ITEM_MANAGER (self));
+  g_return_if_fail (mode != PHOTOS_WINDOW_MODE_NONE);
+  g_return_if_fail (mode != PHOTOS_WINDOW_MODE_EDIT);
+  g_return_if_fail (mode != PHOTOS_WINDOW_MODE_PREVIEW);
+
+  item_mngr_chld = self->item_mngr_chldrn[mode];
+  items = photos_base_manager_get_objects (item_mngr_chld);
+
+  g_hash_table_iter_init (&iter, items);
+  while (g_hash_table_iter_next (&iter, (gpointer *) &id, NULL))
+    {
+      PhotosBaseItem *item = NULL;
+      guint i;
+
+      for (i = 1; self->item_mngr_chldrn[i] != NULL; i++)
+        {
+          if (item_mngr_chld == self->item_mngr_chldrn[i])
+            continue;
+
+          item = PHOTOS_BASE_ITEM (photos_base_manager_get_object_by_id (self->item_mngr_chldrn[i], id));
+          if (item != NULL)
+            break;
+        }
+
+      if (item != NULL)
+        {
+          const gchar *id1;
+
+          id1 = photos_filterable_get_id (PHOTOS_FILTERABLE (item));
+          g_assert_cmpstr (id, ==, id1);
+
+          photos_base_manager_remove_object_by_id (self->item_mngr_chldrn[0], id);
+        }
+    }
+
+  photos_base_manager_clear (item_mngr_chld);
+}
+
+
 PhotosBaseItem *
 photos_item_manager_create_item (PhotosItemManager *self, TrackerSparqlCursor *cursor)
 {

src/photos-item-manager.h
@@ -93,6 +93,9 @@ void                      photos_item_manager_add_item_for_mode            (Phot
                                                                             PhotosWindowMode mode,
                                                                             TrackerSparqlCursor *cursor);
 
+void                      photos_item_manager_clear                        (PhotosItemManager *self,
+                                                                            PhotosWindowMode mode);
+
 PhotosBaseItem           *photos_item_manager_create_item                  (PhotosItemManager *self,
                                                                             TrackerSparqlCursor *cursor);
 

commit 4a72c1a50c3e87d00d87e8f69d0652d86c757545

item-manager: Remove from the 0th child if it doesn't exist anywhere
... and not the other way round.

We will re-introduce the assertion in the subsequent commit so that it
is easier to read.

Fallout from 8d1d00232a9e62deb93ae4d4f498d0b51b3dad2e

https://bugzilla.gnome.org/show_bug.cgi?id=770342

src/photos-item-manager.c
@@ -862,15 +862,8 @@ photos_item_manager_clear (PhotosItemManager *self, PhotosWindowMode mode)
             break;
         }
 
-      if (item != NULL)
-        {
-          const gchar *id1;
-
-          id1 = photos_filterable_get_id (PHOTOS_FILTERABLE (item));
-          g_assert_cmpstr (id, ==, id1);
-
-          photos_base_manager_remove_object_by_id (self->item_mngr_chldrn[0], id);
-        }
+      if (item == NULL)
+        photos_base_manager_remove_object_by_id (self->item_mngr_chldrn[0], id);
     }
 
   photos_base_manager_clear (item_mngr_chld);