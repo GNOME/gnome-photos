GNOME/gnome-photos

commit 0bbe47e4ac23e0c8e3d15dca1916dddc53a35da8

gegl, utils: Move the registration of the built-in GEGL operations
We will need to register the built-in operations in the thumbnailer
too. There are too many build dependencies associated with
photos-utils.[ch] that we can't have in the thumbnailer.

src/photos-utils.c
@@ -40,6 +40,7 @@
 #include "photos-error.h"
 #include "photos-facebook-item.h"
 #include "photos-flickr-item.h"
+#include "photos-gegl.h"
 #include "photos-google-item.h"
 #include "photos-local-item.h"
 #include "photos-media-server-item.h"
@@ -656,6 +657,7 @@ photos_utils_ensure_builtins (void)
   static gsize once_init_value = 0;
 
   photos_utils_ensure_extension_points ();
+  photos_gegl_ensure_builtins ();
 
   if (g_once_init_enter (&once_init_value))
     {
@@ -664,15 +666,6 @@ photos_utils_ensure_builtins (void)
       g_type_ensure (PHOTOS_TYPE_GOOGLE_ITEM);
       g_type_ensure (PHOTOS_TYPE_LOCAL_ITEM);
       g_type_ensure (PHOTOS_TYPE_MEDIA_SERVER_ITEM);
-
-      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_CURVE);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_FILTER);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_HEFE);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_HEFE_CURVE);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_HEFE_VIGNETTE);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_JPG_GUESS_SIZES);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_PNG_GUESS_SIZES);
-      g_type_ensure (PHOTOS_TYPE_OPERATION_SATURATION);
 
       g_type_ensure (PHOTOS_TYPE_SHARE_POINT_EMAIL);
       g_type_ensure (PHOTOS_TYPE_SHARE_POINT_GOOGLE);

src/photos-gegl.c
@@ -23,6 +23,14 @@
 
 #include "photos-debug.h"
 #include "photos-gegl.h"
+#include "photos-operation-insta-curve.h"
+#include "photos-operation-insta-filter.h"
+#include "photos-operation-insta-hefe.h"
+#include "photos-operation-insta-hefe-curve.h"
+#include "photos-operation-insta-hefe-vignette.h"
+#include "photos-operation-jpg-guess-sizes.h"
+#include "photos-operation-png-guess-sizes.h"
+#include "photos-operation-saturation.h"
 #include "photos-quarks.h"
 
 
@@ -191,6 +199,27 @@ photos_gegl_dup_buffer_from_node (GeglNode *node, const Babl *format)
   photos_debug (PHOTOS_DEBUG_GEGL, "GEGL: Dup Buffer from Node: %" G_GINT64_FORMAT, end - start);
 
   return buffer;
+}
+
+
+void
+photos_gegl_ensure_builtins (void)
+{
+  static gsize once_init_value = 0;
+
+  if (g_once_init_enter (&once_init_value))
+    {
+      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_CURVE);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_FILTER);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_HEFE);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_HEFE_CURVE);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_HEFE_VIGNETTE);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_JPG_GUESS_SIZES);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_PNG_GUESS_SIZES);
+      g_type_ensure (PHOTOS_TYPE_OPERATION_SATURATION);
+
+      g_once_init_leave (&once_init_value, 1);
+    }
 }
 
 

src/photos-gegl.h
@@ -43,6 +43,8 @@ GdkPixbuf       *photos_gegl_create_pixbuf_from_node      (GeglNode *node);
 
 GeglBuffer      *photos_gegl_dup_buffer_from_node         (GeglNode *node, const Babl *format);
 
+void             photos_gegl_ensure_builtins              (void);
+
 GeglBuffer      *photos_gegl_get_buffer_from_node         (GeglNode *node, const Babl *format);
 
 void             photos_gegl_processor_process_async      (GeglProcessor *processor,

commit 2440d5881d4a53968aafa4b17213fde2d2c45dfe

utils: Remove redundant headers
Fallout from 0bbe47e4ac23e0c8e3d15dca1916dddc53a35da8

src/photos-utils.c
@@ -48,14 +48,6 @@
 #include "photos-offset-favorites-controller.h"
 #include "photos-offset-overview-controller.h"
 #include "photos-offset-search-controller.h"
-#include "photos-operation-insta-curve.h"
-#include "photos-operation-insta-filter.h"
-#include "photos-operation-insta-hefe.h"
-#include "photos-operation-insta-hefe-curve.h"
-#include "photos-operation-insta-hefe-vignette.h"
-#include "photos-operation-jpg-guess-sizes.h"
-#include "photos-operation-png-guess-sizes.h"
-#include "photos-operation-saturation.h"
 #include "photos-query.h"
 #include "photos-share-point.h"
 #include "photos-share-point-email.h"