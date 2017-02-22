1b5626adfa911d6276c60522d3bead60ee5b38c2
Debarshi Ray <debarshir@gnome.org>
2017-02-22 16:37:38 +0100

    base-item: Style fix

--- src/photos-base-item.c
+++ src/photos-base-item.c
@@ -441,20 +441,18 @@ photos_base_item_create_thumbnail_in_thread_func (gpointer data, gpointer user_d
   PhotosBaseItem *self;
   GCancellable *cancellable;
   GError *error;
-  gboolean result;
 
   self = PHOTOS_BASE_ITEM (g_task_get_source_object (task));
   cancellable = g_task_get_cancellable (task);
 
   error = NULL;
-  result = PHOTOS_BASE_ITEM_GET_CLASS (self)->create_thumbnail (self, cancellable, &error);
-  if (error != NULL)
+  if (!PHOTOS_BASE_ITEM_GET_CLASS (self)->create_thumbnail (self, cancellable, &error))
     {
       g_task_return_error (task, error);
       goto out;
     }
 
-  g_task_return_boolean (task, result);
+  g_task_return_boolean (task, TRUE);
 
  out:
   g_object_unref (task);
