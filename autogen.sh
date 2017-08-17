10e95d25edf2a2e8744e51697c32a2c4425d8901
Debarshi Ray <debarshir@gnome.org>
2017-08-17 11:32:24 +0200

    build: Remove workaround for Automake 1.13.2 bug
    Automake 1.13.2 is not so recent anymore and the workaround is absent
    from the sample template used in:
    https://wiki.gnome.org/Projects/GnomeCommon/Migration

--- autogen.sh
+++ autogen.sh
@@ -24,14 +24,6 @@ fi
 
 git submodule update --init --recursive
 
-# if the AC_CONFIG_MACRO_DIR() macro is used, create that directory
-# This is a automake bug fixed in automake 1.13.2
-# See http://debbugs.gnu.org/cgi/bugreport.cgi?bug=13514
-m4dir=`autoconf --trace 'AC_CONFIG_MACRO_DIR:$1'`
-if [ -n "$m4dir" ]; then
-    mkdir -p $m4dir
-fi
-
 set -x
 
 intltoolize --force --copy --automake || exit 1
