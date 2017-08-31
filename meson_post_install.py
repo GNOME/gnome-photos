cc5584327aacd6590ff69eeb5a130c41843ce3c8
Iñigo Martínez <inigomartinez@gmail.com>
2017-08-31 18:50:13 +0200

    build: Port to Meson build system
    Meson is a build system focused on speed and ease of use, which
    helps speed up software development. This patch adds Meson support
    alongside Autotools.
    
    Some changes by Debarshi Ray.
    
    https://gitlab.gnome.org/GNOME/gnome-photos/merge_requests/48
    https://gitlab.gnome.org/GNOME/gnome-photos/merge_requests/51

---
+++ meson_post_install.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import re
+import subprocess
+import sys
+
+destdir = os.environ.get('DESTDIR', '')
+
+if destdir:
+    datadir = os.path.normpath(destdir + os.sep + sys.argv[1])
+else:
+    datadir = sys.argv[1]
+
+icondir = os.path.join(datadir, 'icons', 'hicolor')
+
+name_pattern = re.compile('hicolor_(?:apps)_(?:\d+x\d+|scalable)_(.*)')
+search_pattern = '/**/hicolor_*'
+
+for file in glob.glob(icondir + search_pattern, recursive=True):
+    os.rename(file, os.path.join(os.path.dirname(file), name_pattern.search(file).group(1)))
+
+if not destdir:
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
+
+    schemadir = os.path.join(datadir, 'glib-2.0', 'schemas')
+    print('Compiling gsettings schemas...')
+    subprocess.call(['glib-compile-schemas', schemadir])
