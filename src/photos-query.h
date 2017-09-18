GNOME/gnome-photos

commit cd2ba0c4f371968f1062449c27a5a8ae01df4d1a

query-builder: Implement photos_query_builder_global_query

src/photos-query.h
@@ -21,6 +21,11 @@
 #ifndef PHOTOS_QUERY_H
 #define PHOTOS_QUERY_H
 
+#include <glib.h>
+
+#include "photos-query.h"
+#include "photos-source.h"
+
 G_BEGIN_DECLS
 
 typedef enum
@@ -38,6 +43,24 @@ typedef enum
   PHOTOS_QUERY_COLUMNS_RESOURCE_FAVORITE,
   PHOTOS_QUERY_COLUMNS_RESOURCE_SHARED
 } PhotosQueryColumns;
+
+typedef enum
+{
+  PHOTOS_QUERY_FLAGS_NONE = 0,
+  PHOTOS_QUERY_FLAGS_UNFILTERED = 1 << 0
+} PhotosQueryFlags;
+
+typedef struct _PhotosQuery PhotosQuery;
+
+struct _PhotosQuery
+{
+  PhotosSource *source;
+  gchar *sparql;
+};
+
+PhotosQuery    *photos_query_new     (gchar *sparql);
+
+void            photos_query_free    (PhotosQuery *query);
 
 G_END_DECLS
 

src/Makefile.am
@@ -51,6 +51,7 @@ gnome_photos_SOURCES = \
 	photos-organize-collection-model.h \
 	photos-organize-collection-view.c \
 	photos-organize-collection-view.h \
+	photos-query.c \
 	photos-query.h \
 	photos-query-builder.h \
 	photos-query-builder.c \

src/photos-query-builder.c
@@ -23,6 +23,7 @@
 
 #include <gio/gio.h>
 
+#include "photos-offset-controller.h"
 #include "photos-query-builder.h"
 
 
@@ -40,6 +41,87 @@ photos_query_builder_convert_path_to_uri (const gchar *path)
   g_object_unref (file);
 
   return uri;
+}
+
+
+static gchar *
+photos_query_builder_optional (void)
+{
+  return g_strdup ("OPTIONAL { ?urn nco:creator ?creater . } "
+                   "OPTIONAL { ?urn nco:publisher ?publisher . }");
+}
+
+
+static gchar *
+photos_query_builder_query (gboolean global, gint flags)
+{
+  gchar *global_sparql;
+  gchar *optional;
+  gchar *sparql;
+  gchar *tmp;
+
+  optional = photos_query_builder_optional ();
+  global_sparql = g_strconcat ("WHERE { ?urn a rdfs:Resource ", optional, NULL);
+
+  if (global)
+    {
+      PhotosOffsetController *offset_cntrlr;
+      gint offset;
+      gint step;
+
+      if (!(flags & PHOTOS_QUERY_FLAGS_UNFILTERED))
+        {
+        }
+
+      offset_cntrlr = photos_offset_controller_new ();
+      offset = photos_offset_controller_get_offset (offset_cntrlr);
+      step = photos_offset_controller_get_step (offset_cntrlr);
+      g_object_unref (offset_cntrlr);
+
+      tmp = global_sparql;
+      global_sparql = g_strdup_printf ("%s } ORDER BY DESC (?mtime) LIMIT %d OFFSET %d",
+                                       global_sparql,
+                                       step,
+                                       offset);
+      g_free (tmp);
+    }
+  else
+    {
+      if (!(flags & PHOTOS_QUERY_FLAGS_UNFILTERED))
+        {
+        }
+
+      tmp = global_sparql;
+      global_sparql = g_strconcat (global_sparql, " }", NULL);
+      g_free (tmp);
+    }
+
+  sparql = g_strconcat ("SELECT DISTINCT ?urn "
+                        "nie:url (?urn) "
+                        "nfo:fileName (?urn) "
+                        "nie:mimeType (?urn) "
+                        "nie:title (?urn) "
+                        "tracker:coalesce (nco:fullname (?creator), nco:fullname (?publisher), '') "
+                        "tracker:coalesce (nfo:fileLastModified (?urn), nie:contentLastModified (?urn)) AS ?mtime "
+                        "nao:identifier (?urn) "
+                        "rdf:type (?urn) "
+                        "nie:dataSource(?urn) "
+                        "( EXISTS { ?urn nao:hasTag nao:predefined-tag-favorite } ) "
+                        "( EXISTS { ?urn nco:contributor ?contributor FILTER ( ?contributor != ?creator ) } ) ",
+                        global_sparql,
+                        NULL);
+  g_free (global_sparql);
+  return sparql;
+}
+
+
+PhotosQuery *
+photos_query_builder_global_query (void)
+{
+  gchar *sparql;
+
+  sparql = photos_query_builder_query (TRUE, PHOTOS_QUERY_FLAGS_NONE);
+  return photos_query_new (sparql);
 }
 
 

src/photos-query.c
@@ -18,17 +18,35 @@
  * 02110-1301, USA.
  */
 
-#ifndef PHOTOS_QUERY_BUILDER_H
-#define PHOTOS_QUERY_BUILDER_H
+
+#include "config.h"
 
 #include "photos-query.h"
+#include "photos-source-manager.h"
 
-G_BEGIN_DECLS
 
-PhotosQuery  *photos_query_builder_global_query        (void);
+PhotosQuery *
+photos_query_new (gchar *sparql)
+{
+  PhotosBaseManager *src_mngr;
+  PhotosQuery *query;
 
-gchar        *photos_query_builder_filter_local        (void);
+  query = g_slice_new0 (PhotosQuery);
 
-G_END_DECLS
+  src_mngr = photos_source_manager_new ();
+  query->source = PHOTOS_SOURCE (photos_base_manager_get_active_object (src_mngr));
+  g_object_unref (src_mngr);
 
-#endif /* PHOTOS_QUERY_BUILDER */
+  query->sparql = sparql;
+
+  return query;
+}
+
+
+void
+photos_query_free (PhotosQuery *query)
+{
+  g_object_unref (query->source);
+  g_free (query->sparql);
+  g_slice_free (PhotosQuery, query);
+}

commit 912085f318767bdb0912075534d6623ab1c7c34a

query: Fix typo
A header shouldn't include itself.

Fallout from cd2ba0c4f371968f1062449c27a5a8ae01df4d1a

src/photos-query.h
@@ -27,7 +27,6 @@
 
 #include <glib.h>
 
-#include "photos-query.h"
 #include "photos-search-context.h"
 #include "photos-source.h"
 