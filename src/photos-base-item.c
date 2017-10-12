GNOME/gnome-photos

commit 8047f788f51cc551e57b012372455154eb57f739

base-item: Load only the pipeline to determine if its edited or not
Loading the entire item is considerably more expensive in terms of CPU
and I/O than loading just the pipeline.

src/photos-base-item.c
@@ -1699,29 +1699,27 @@ photos_base_item_metadata_add_shared_in_thread_func (GTask *task,
 
 
 static void
-photos_base_item_pipeline_is_edited_load (GObject *source_object, GAsyncResult *res, gpointer user_data)
+photos_base_item_pipeline_is_edited_load_pipeline (GObject *source_object, GAsyncResult *res, gpointer user_data)
 {
   PhotosBaseItem *self = PHOTOS_BASE_ITEM (source_object);
   GError *error;
   GTask *task = G_TASK (user_data);
-  GeglNode *graph = NULL;
-  PhotosPipeline *pipeline;
+  PhotosPipeline *pipeline = NULL;
   gboolean is_edited;
 
   error = NULL;
-  graph = photos_base_item_load_finish (self, res, &error);
+  pipeline = photos_base_item_load_pipeline_finish (self, res, &error);
   if (error != NULL)
     {
       g_task_return_error (task, error);
       goto out;
     }
 
-  pipeline = PHOTOS_PIPELINE (egg_task_cache_peek (pipeline_cache, self));
   is_edited = photos_pipeline_is_edited (pipeline);
   g_task_return_boolean (task, is_edited);
 
  out:
-  g_clear_object (&graph);
+  g_clear_object (&pipeline);
   g_object_unref (task);
 }
 
@@ -3771,7 +3769,10 @@ photos_base_item_pipeline_is_edited_async (PhotosBaseItem *self,
   task = g_task_new (self, cancellable, callback, user_data);
   g_task_set_source_tag (task, photos_base_item_pipeline_is_edited_async);
 
-  photos_base_item_load_async (self, cancellable, photos_base_item_pipeline_is_edited_load, g_object_ref (task));
+  photos_base_item_load_pipeline_async (self,
+                                        cancellable,
+                                        photos_base_item_pipeline_is_edited_load_pipeline,
+                                        g_object_ref (task));
 
   g_object_unref (task);
 }

commit eb8d3deae4424378d549a6d3e4960f6e4192f0fd

base-item: Process the graph after a revert only if the item is loaded
Commit 8047f788f51cc551 decoupled the loading of the Pipeline from the
rest of the BaseItem. The presence of a pipeline doesn't imply the
existence of a buffer source, edit graph, etc..

Therefore, discarding all edits from the overview, without loading the
BaseItem, would lead to processing a GEGL graph without any input and:
  GEGL-WARNING **: Output of gegl:nop 'proxynop-output' 0x7f741c026900
    has no format

Processing a BaseItem after changing its Pipeline is only necessary if
the rest of the BaseItem is loaded. Hence it is better to just skip it
in this case.

Note that the thumbnail is updated when the Pipeline is saved, which
also doesn't require loading the BaseItem. The thumbnailer takes care
of that.

Fallout from 8047f788f51cc551e57b012372455154eb57f739

https://bugzilla.gnome.org/show_bug.cgi?id=788715

src/photos-base-item.c
@@ -4018,7 +4018,7 @@ photos_base_item_pipeline_revert_async (PhotosBaseItem *self,
                                         gpointer user_data)
 {
   PhotosBaseItemPrivate *priv;
-  GTask *task;
+  GTask *task = NULL;
   PhotosPipeline *pipeline;
 
   g_return_if_fail (PHOTOS_IS_BASE_ITEM (self));
@@ -4034,9 +4034,16 @@ photos_base_item_pipeline_revert_async (PhotosBaseItem *self,
   task = g_task_new (self, cancellable, callback, user_data);
   g_task_set_source_tag (task, photos_base_item_pipeline_revert_async);
 
+  if (priv->edit_graph == NULL)
+    {
+      g_task_return_boolean (task, TRUE);
+      goto out;
+    }
+
   photos_base_item_process_async (self, cancellable, photos_base_item_common_process, g_object_ref (task));
 
-  g_object_unref (task);
+ out:
+  g_clear_object (&task);
 }
 
 
@@ -4062,7 +4069,7 @@ photos_base_item_pipeline_revert_to_original_async (PhotosBaseItem *self,
 
 {
   PhotosBaseItemPrivate *priv;
-  GTask *task;
+  GTask *task = NULL;
   PhotosPipeline *pipeline;
 
   g_return_if_fail (PHOTOS_IS_BASE_ITEM (self));
@@ -4078,9 +4085,16 @@ photos_base_item_pipeline_revert_to_original_async (PhotosBaseItem *self,
   task = g_task_new (self, cancellable, callback, user_data);
   g_task_set_source_tag (task, photos_base_item_pipeline_revert_to_original_async);
 
+  if (priv->edit_graph == NULL)
+    {
+      g_task_return_boolean (task, TRUE);
+      goto out;
+    }
+
   photos_base_item_process_async (self, cancellable, photos_base_item_common_process, g_object_ref (task));
 
-  g_object_unref (task);
+ out:
+  g_clear_object (&task);
 }
 
 