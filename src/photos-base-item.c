GNOME/gnome-photos

commit 8047f788f51cc551e57b012372455154eb57f739

base-item: Load only the pipeline to determine if its edited or not
Loading the entire item is considerably more expensive in terms of CPU
and I/O than loading just the pipeline.

src/photos-base-item.c
@@ -1699,29 +1699,27 @@ photos_base_item_metadata_add_shared_in_thread_func (GTask *task,
 
 
 static void
-photos_base_item_pipeline_is_edited_load (GObject *source_object, GAsyncResult *res, gpointer user_data)
+photos_base_item_pipeline_is_edited_load_pipeline (GObject *source_object, GAsyncResult *res, gpointer user_data)
 {
   PhotosBaseItem *self = PHOTOS_BASE_ITEM (source_object);
   GError *error;
   GTask *task = G_TASK (user_data);
-  GeglNode *graph = NULL;
-  PhotosPipeline *pipeline;
+  PhotosPipeline *pipeline = NULL;
   gboolean is_edited;
 
   error = NULL;
-  graph = photos_base_item_load_finish (self, res, &error);
+  pipeline = photos_base_item_load_pipeline_finish (self, res, &error);
   if (error != NULL)
     {
       g_task_return_error (task, error);
       goto out;
     }
 
-  pipeline = PHOTOS_PIPELINE (egg_task_cache_peek (pipeline_cache, self));
   is_edited = photos_pipeline_is_edited (pipeline);
   g_task_return_boolean (task, is_edited);
 
  out:
-  g_clear_object (&graph);
+  g_clear_object (&pipeline);
   g_object_unref (task);
 }
 
@@ -3771,7 +3769,10 @@ photos_base_item_pipeline_is_edited_async (PhotosBaseItem *self,
   task = g_task_new (self, cancellable, callback, user_data);
   g_task_set_source_tag (task, photos_base_item_pipeline_is_edited_async);
 
-  photos_base_item_load_async (self, cancellable, photos_base_item_pipeline_is_edited_load, g_object_ref (task));
+  photos_base_item_load_pipeline_async (self,
+                                        cancellable,
+                                        photos_base_item_pipeline_is_edited_load_pipeline,
+                                        g_object_ref (task));
 
   g_object_unref (task);
 }

commit 7b47708d8f6fa968748f90e4a2e0e811db328730

base-item: Unbreak reverting directly from OVERVIEW without PREVIEW
Commit 8047f788f51cc551 decoupled the loading of the Pipeline from the
rest of the BaseItem. The presence of a pipeline doesn't imply the
existence of a buffer source, edit graph, etc..

Therefore, discarding all edits from the overview, without loading the
BaseItem, would lead to:
  CRITICAL **: photos_base_item_pipeline_save_async: assertion
    'priv->edit_graph != NULL' failed

This prevented the thumbnail from getting updated and the revert
wouldn't be persistent.

Since saving a Pipeline doesn't even require a fully loaded BaseItem,
it is enough to just remove this anachronistic and overzealous
assertion.

Fallout from 8047f788f51cc551e57b012372455154eb57f739

https://bugzilla.gnome.org/show_bug.cgi?id=788715

src/photos-base-item.c
@@ -4126,7 +4126,6 @@ photos_base_item_pipeline_save_async (PhotosBaseItem *self,
   priv = photos_base_item_get_instance_private (self);
 
   g_return_if_fail (!priv->collection);
-  g_return_if_fail (priv->edit_graph != NULL);
 
   pipeline = PHOTOS_PIPELINE (egg_task_cache_peek (pipeline_cache, self));
   g_return_if_fail (pipeline != NULL);