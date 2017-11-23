GNOME/gnome-photos

commit a2ae6b14f5dedcc7202d3e957e8b0a236d475c9d

Add Instagram Hefe
The filter is implemented by a hidden meta-operation called
photos:insta-hefe, which in turn uses photos:insta-hefe-curve,
photos:insta-hefe-vignette and svg:multiply.

Based on an analysis by Corey Hoard <corey@coreyhoard.com>:
http://www.coreyhoard.com/mirror/CH_Rev_Eng_Hefe_wDataset.zip

src/photos-operation-insta-hefe-vignette.c
@@ -1,0 +1,290 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2016 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+
+#include "config.h"
+
+#include <babl/babl.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+#include <gegl.h>
+#include <gegl-plugin.h>
+
+#include "photos-operation-insta-hefe-vignette.h"
+
+
+struct _PhotosOperationInstaHefeVignette
+{
+  GeglOperationPointRender parent_instance;
+  gdouble height;
+  gdouble height_ratio;
+  gdouble width;
+  gdouble width_ratio;
+  gdouble x;
+  gdouble y;
+};
+
+struct _PhotosOperationInstaHefeVignetteClass
+{
+  GeglOperationPointRenderClass parent_class;
+};
+
+enum
+{
+  PROP_0,
+  PROP_HEIGHT,
+  PROP_WIDTH,
+  PROP_X,
+  PROP_Y
+};
+
+
+G_DEFINE_TYPE (PhotosOperationInstaHefeVignette,
+               photos_operation_insta_hefe_vignette,
+               GEGL_TYPE_OPERATION_POINT_RENDER);
+
+
+static GdkPixbuf *vignette;
+static guchar *vignette_pixels;
+static gint vignette_channels;
+static gint vignette_height;
+static gint vignette_rowstride;
+static gint vignette_width;
+
+
+static void
+photos_operation_insta_hefe_vignette_get_rgb (PhotosOperationInstaHefeVignette *self,
+                                              gint x,
+                                              gint y,
+                                              guint8 *out_r,
+                                              guint8 *out_g,
+                                              guint8 *out_b)
+{
+  gint pixbuf_x;
+  gint pixbuf_y;
+  guchar *pixel;
+
+  pixbuf_x = (gint) (((gdouble) x - self->x) * self->width_ratio + 0.5);
+  pixbuf_y = (gint) (((gdouble) y - self->y) * self->height_ratio + 0.5);
+  pixel = vignette_pixels + (vignette_rowstride * pixbuf_y) + vignette_channels * pixbuf_x;
+
+  /* We don't want optional out parameters because they will
+   * introduce if branches in this hot path.
+  */
+  *out_r = (guint8) pixel[0];
+  *out_g = (guint8) pixel[1];
+  *out_b = (guint8) pixel[2];
+}
+
+
+static GeglRectangle
+photos_operation_insta_hefe_vignette_get_bounding_box (GeglOperation *operation)
+{
+  PhotosOperationInstaHefeVignette *self = PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE (operation);
+  GeglRectangle bbox;
+
+  gegl_rectangle_set (&bbox, (gint) self->x, (gint) self->y, (gint) self->width, (gint) self->height);
+  return bbox;
+}
+
+
+static void
+photos_operation_insta_hefe_vignette_prepare (GeglOperation *operation)
+{
+  const Babl* format;
+
+  format = babl_format ("R'G'B'A u8");
+  gegl_operation_set_format (operation, "output", format);
+}
+
+
+static gboolean
+photos_operation_insta_hefe_vignette_process (GeglOperation *operation,
+                                              void *out_buf,
+                                              glong n_pixels,
+                                              const GeglRectangle *roi,
+                                              gint level)
+{
+  PhotosOperationInstaHefeVignette *self = PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE (operation);
+  const gint x1 = roi->x + roi->width;
+  const gint y1 = roi->y + roi->height;
+  gint x;
+  gint y;
+  guint8 *out = out_buf;
+
+  for (y = roi->y; y < y1; y++)
+    {
+      for (x = roi->x; x < x1; x++)
+        {
+          photos_operation_insta_hefe_vignette_get_rgb (self, x, y, &out[0], &out[1], &out[2]);
+          out[3] = 255;
+          out += 4;
+        }
+    }
+
+  return TRUE;
+}
+
+
+static void
+photos_operation_insta_hefe_vignette_get_property (GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
+{
+  PhotosOperationInstaHefeVignette *self = PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE (object);
+
+  switch (prop_id)
+    {
+    case PROP_HEIGHT:
+      g_value_set_double (value, self->height);
+      break;
+
+    case PROP_WIDTH:
+      g_value_set_double (value, self->width);
+      break;
+
+    case PROP_X:
+      g_value_set_double (value, self->x);
+      break;
+
+    case PROP_Y:
+      g_value_set_double (value, self->y);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+
+static void
+photos_operation_insta_hefe_vignette_set_property (GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
+{
+  PhotosOperationInstaHefeVignette *self = PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE (object);
+
+  switch (prop_id)
+    {
+    case PROP_HEIGHT:
+      self->height = g_value_get_double (value);
+      self->height_ratio = vignette_height / self->height;
+      break;
+
+    case PROP_WIDTH:
+      self->width = g_value_get_double (value);
+      self->width_ratio = vignette_width / self->width;
+      break;
+
+    case PROP_X:
+      self->x = g_value_get_double (value);
+      break;
+
+    case PROP_Y:
+      self->y = g_value_get_double (value);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+
+static void
+photos_operation_insta_hefe_vignette_init (PhotosOperationInstaHefeVignette *self)
+{
+  if (vignette == NULL)
+    {
+      GError *error;
+
+      error = NULL;
+      vignette = gdk_pixbuf_new_from_resource ("/org/gnome/Photos/vignette.png", &error);
+      g_assert_no_error (error);
+
+      vignette_channels = gdk_pixbuf_get_n_channels (vignette);
+      g_assert_cmpint (vignette_channels, ==, 3);
+
+      vignette_pixels = gdk_pixbuf_get_pixels (vignette);
+      vignette_rowstride = gdk_pixbuf_get_rowstride (vignette);
+      vignette_height = gdk_pixbuf_get_height (vignette);
+      vignette_width = gdk_pixbuf_get_width (vignette);
+    }
+}
+
+
+static void
+photos_operation_insta_hefe_vignette_class_init (PhotosOperationInstaHefeVignetteClass *class)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
+  GeglOperationClass *operation_class = GEGL_OPERATION_CLASS (class);
+  GeglOperationPointRenderClass *point_render_class = GEGL_OPERATION_POINT_RENDER_CLASS (class);
+
+  operation_class->opencl_support = FALSE;
+
+  object_class->get_property = photos_operation_insta_hefe_vignette_get_property;
+  object_class->set_property = photos_operation_insta_hefe_vignette_set_property;
+  operation_class->get_bounding_box = photos_operation_insta_hefe_vignette_get_bounding_box;
+  operation_class->prepare = photos_operation_insta_hefe_vignette_prepare;
+  point_render_class->process = photos_operation_insta_hefe_vignette_process;
+
+  g_object_class_install_property (object_class,
+                                   PROP_HEIGHT,
+                                   g_param_spec_double ("height",
+                                                        "Height",
+                                                        "Vertical extent",
+                                                        0.0,
+                                                        G_MAXDOUBLE,
+                                                        10.0,
+                                                        G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
+
+  g_object_class_install_property (object_class,
+                                   PROP_WIDTH,
+                                   g_param_spec_double ("width",
+                                                        "Width",
+                                                        "Horizontal extent",
+                                                        0.0,
+                                                        G_MAXDOUBLE,
+                                                        10.0,
+                                                        G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
+
+  g_object_class_install_property (object_class,
+                                   PROP_X,
+                                   g_param_spec_double ("x",
+                                                        "X",
+                                                        "Horizontal position",
+                                                        0.0,
+                                                        G_MAXDOUBLE,
+                                                        0.0,
+                                                        G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
+
+  g_object_class_install_property (object_class,
+                                   PROP_Y,
+                                   g_param_spec_double ("y",
+                                                        "Y",
+                                                        "Vertical position",
+                                                        0.0,
+                                                        G_MAXDOUBLE,
+                                                        0.0,
+                                                        G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
+
+  gegl_operation_class_set_keys (operation_class,
+                                 "name", "photos:insta-hefe-vignette",
+                                 "title", "Insta Hefe Vignette",
+                                 "description", "Apply the Hefe vignette to an image",
+                                 "categories", "hidden",
+                                 NULL);
+}

data/Makefile.am
@@ -35,6 +35,7 @@ EXTRA_DIST = \
 	sidebar-radio-selected.svg \
 	sidebar-radio-selected-prelight.svg \
 	thumbnail-frame.png \
+	vignette.png \
 	$(NULL)
 
 CLEANFILES = \

src/Makefile.am
@@ -132,6 +132,12 @@ gnome_photos_SOURCES = \
 	photos-operation-insta-curve.h \
 	photos-operation-insta-filter.c \
 	photos-operation-insta-filter.h \
+	photos-operation-insta-hefe.c \
+	photos-operation-insta-hefe.h \
+	photos-operation-insta-hefe-curve.c \
+	photos-operation-insta-hefe-curve.h \
+	photos-operation-insta-hefe-vignette.c \
+	photos-operation-insta-hefe-vignette.h \
 	photos-operation-jpg-guess-sizes.c \
 	photos-operation-jpg-guess-sizes.h \
 	photos-operation-png-guess-sizes.c \

src/photos-operation-insta-curve.c
@@ -668,6 +668,7 @@ photos_operation_insta_curve_prepare (GeglOperation *operation)
 
     case PHOTOS_OPERATION_INSTA_PRESET_NONE:
     case PHOTOS_OPERATION_INSTA_PRESET_GRAY:
+    case PHOTOS_OPERATION_INSTA_PRESET_HEFE:
     default:
       g_assert_not_reached ();
     }

src/photos-operation-insta-filter.c
@@ -111,6 +111,13 @@ photos_operation_insta_filter_setup (PhotosOperationInstaFilter *self)
       self->nodes = g_list_prepend (self->nodes, node);
       break;
 
+    case PHOTOS_OPERATION_INSTA_PRESET_HEFE:
+      node = gegl_node_new_child (operation->node,
+                                  "operation", "photos:insta-hefe",
+                                  NULL);
+      self->nodes = g_list_prepend (self->nodes, node);
+      break;
+
     case PHOTOS_OPERATION_INSTA_PRESET_NASHVILLE:
       node = gegl_node_new_child (operation->node,
                                   "operation", "photos:insta-curve",

src/photos-operation-insta-hefe-curve.c
@@ -1,0 +1,116 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2016 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+
+#include "config.h"
+
+#include <babl/babl.h>
+#include <gegl.h>
+#include <gegl-plugin.h>
+
+#include "photos-operation-insta-hefe-curve.h"
+
+
+struct _PhotosOperationInstaHefeCurve
+{
+  GeglOperationPointFilter parent_instance;
+};
+
+struct _PhotosOperationInstaHefeCurveClass
+{
+  GeglOperationPointFilterClass parent_class;
+};
+
+
+G_DEFINE_TYPE (PhotosOperationInstaHefeCurve, photos_operation_insta_hefe_curve, GEGL_TYPE_OPERATION_POINT_FILTER);
+
+
+static void
+photos_operation_insta_hefe_curve_prepare (GeglOperation *operation)
+{
+  const Babl *format;
+
+  format = babl_format ("R'G'B'A float");
+  gegl_operation_set_format (operation, "input", format);
+  gegl_operation_set_format (operation, "output", format);
+}
+
+
+static gboolean
+photos_operation_insta_hefe_curve_process (GeglOperation *operation,
+                                           void *in_buf,
+                                           void *out_buf,
+                                           glong n_pixels,
+                                           const GeglRectangle *roi,
+                                           gint level)
+{
+  gfloat *in = in_buf;
+  gfloat *out = out_buf;
+  glong i;
+
+  for (i = 0; i < n_pixels; i++)
+    {
+      const float b = in[2];
+      const float b2 = b * b;
+      const float b3 = b2 * b;
+      const float g = in[1];
+      const float g2 = g * g;
+      const float g3 = g2 * g;
+      const float r = in[0];
+      const float r2 = r * r;
+      const float r3 = r2 * r;
+
+      out[0] = -13.47f * r3 * r3 + 41.23f * r3 * r2 - 45.04f * r2 * r2 + 19.17 * r3 - 1.492f * r2 + 0.5954f * r;
+      out[1] = -12.28f * g3 * g3 + 41.09f * g3 * g2 - 50.52f * g2 * g2 + 26.03 * g3 - 3.916f * g2 + 0.58f * g;
+      out[2] = -1.066f * b3 * b3 + 9.679f * b3 * b2 - 19.09f * b2 * b2 + 12.92 * b3 - 1.835f * b2 + 0.3487f * b;
+      out[3] = in[3];
+
+      in += 4;
+      out += 4;
+    }
+
+  return TRUE;
+}
+
+
+static void
+photos_operation_insta_hefe_curve_init (PhotosOperationInstaHefeCurve *self)
+{
+}
+
+
+static void
+photos_operation_insta_hefe_curve_class_init (PhotosOperationInstaHefeCurveClass *class)
+{
+  GeglOperationClass *operation_class = GEGL_OPERATION_CLASS (class);
+  GeglOperationPointFilterClass *point_filter_class = GEGL_OPERATION_POINT_FILTER_CLASS (class);
+
+  operation_class->opencl_support = FALSE;
+
+  operation_class->prepare = photos_operation_insta_hefe_curve_prepare;
+  point_filter_class->process = photos_operation_insta_hefe_curve_process;
+
+  gegl_operation_class_set_keys (operation_class,
+                                 "name", "photos:insta-hefe-curve",
+                                 "title", "Insta Hefe Curve",
+                                 "description", "Apply the Hefe curve to an image",
+                                 "categories", "hidden",
+                                 NULL);
+}

src/photos-operation-insta-hefe-curve.h
@@ -1,0 +1,45 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2016 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef PHOTOS_OPERATION_INSTA_HEFE_CURVE_H
+#define PHOTOS_OPERATION_INSTA_HEFE_CURVE_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define PHOTOS_TYPE_OPERATION_INSTA_HEFE_CURVE (photos_operation_insta_hefe_curve_get_type ())
+
+#define PHOTOS_OPERATION_INSTA_HEFE_CURVE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+   PHOTOS_TYPE_OPERATION_INSTA_HEFE_CURVE, PhotosOperationInstaHefeCurve))
+
+#define PHOTOS_IS_OPERATION_INSTA_HEFE_CURVE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+   PHOTOS_TYPE_OPERATION_INSTA_HEFE_CURVE))
+
+typedef struct _PhotosOperationInstaHefeCurve      PhotosOperationInstaHefeCurve;
+typedef struct _PhotosOperationInstaHefeCurveClass PhotosOperationInstaHefeCurveClass;
+
+GType            photos_operation_insta_hefe_curve_get_type       (void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* PHOTOS_OPERATION_INSTA_HEFE_CURVE_H */

src/photos-operation-insta-hefe-vignette.h
@@ -1,0 +1,45 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2016 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE_H
+#define PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define PHOTOS_TYPE_OPERATION_INSTA_HEFE_VIGNETTE (photos_operation_insta_hefe_vignette_get_type ())
+
+#define PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+   PHOTOS_TYPE_OPERATION_INSTA_HEFE_VIGNETTE, PhotosOperationInstaHefeVignette))
+
+#define PHOTOS_IS_OPERATION_INSTA_HEFE_VIGNETTE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+   PHOTOS_TYPE_OPERATION_INSTA_HEFE_VIGNETTE))
+
+typedef struct _PhotosOperationInstaHefeVignette      PhotosOperationInstaHefeVignette;
+typedef struct _PhotosOperationInstaHefeVignetteClass PhotosOperationInstaHefeVignetteClass;
+
+GType            photos_operation_insta_hefe_vignette_get_type       (void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE_H */

commit 67f9b9a18d7cc7a9c512079bf544549138fdeea3

operation-insta-hefe-vignette: Use the correct typecast
The height and width need to be cast to guint, because, unlike
GeglRectangle, gegl_rectangle_set uses unsigned integers for those
parameters.

Fallout from a2ae6b14f5dedcc7202d3e957e8b0a236d475c9d

src/photos-operation-insta-hefe-vignette.c
@@ -93,7 +93,7 @@ photos_operation_insta_hefe_vignette_get_bounding_box (GeglOperation *operation)
   PhotosOperationInstaHefeVignette *self = PHOTOS_OPERATION_INSTA_HEFE_VIGNETTE (operation);
   GeglRectangle bbox;
 
-  gegl_rectangle_set (&bbox, (gint) self->x, (gint) self->y, (gint) self->width, (gint) self->height);
+  gegl_rectangle_set (&bbox, (gint) self->x, (gint) self->y, (guint) self->width, (guint) self->height);
   return bbox;
 }
 