GNOME/gnome-photos

commit 42fc086eabe602fe9bcd4c9c1849efebdbb42601

gegl: Remove unused function
https://bugzilla.gnome.org/show_bug.cgi?id=781736

src/photos-gegl.c
@@ -374,33 +374,6 @@ photos_gegl_buffer_zoom_finish (GeglBuffer *buffer, GAsyncResult *res, GError **
 }
 
 
-GeglNode *
-photos_gegl_create_orientation_node (GeglNode *parent, GQuark orientation)
-{
-  GeglNode *ret_val = NULL;
-  double degrees = 1.0;
-
-  if (orientation == PHOTOS_ORIENTATION_TOP)
-    goto out;
-
-  if (orientation == PHOTOS_ORIENTATION_BOTTOM)
-    degrees = -180.0;
-  else if (orientation == PHOTOS_ORIENTATION_LEFT)
-    degrees = -270.0;
-  else if (orientation == PHOTOS_ORIENTATION_RIGHT)
-    degrees = -90.0;
-
-  if (degrees < 0.0)
-    ret_val = gegl_node_new_child (parent, "operation", "gegl:rotate-on-center", "degrees", degrees, NULL);
-
- out:
-  if (ret_val == NULL)
-    ret_val = gegl_node_new_child (parent, "operation", "gegl:nop", NULL);
-
-  return ret_val;
-}
-
-
 GdkPixbuf *
 photos_gegl_create_pixbuf_from_node (GeglNode *node)
 {

src/photos-gegl.h
@@ -41,8 +41,6 @@ void             photos_gegl_buffer_zoom_async            (GeglBuffer *buffer,
 
 GeglBuffer      *photos_gegl_buffer_zoom_finish           (GeglBuffer *buffer, GAsyncResult *res, GError **error);
 
-GeglNode        *photos_gegl_create_orientation_node      (GeglNode *parent, GQuark orientation);
-
 GdkPixbuf       *photos_gegl_create_pixbuf_from_node      (GeglNode *node);
 
 GeglBuffer      *photos_gegl_dup_buffer_from_node         (GeglNode *node, const Babl *format);

commit c270cc98df7dc8ca2834f5d4a769e47f5d474a18

gegl: Update the list of mandatory operations
gegl:rotate-on-center is no longer used.

Fallout from 42fc086eabe602fe9bcd4c9c1849efebdbb42601

https://bugzilla.gnome.org/show_bug.cgi?id=781736

src/photos-gegl.c
@@ -59,7 +59,6 @@ static const gchar *REQUIRED_GEGL_OPS[] =
   "gegl:load",
   "gegl:noise-reduction",
   "gegl:nop",
-  "gegl:rotate-on-center",
   "gegl:save-pixbuf",
   "gegl:scale-ratio",
   "gegl:unsharp-mask",