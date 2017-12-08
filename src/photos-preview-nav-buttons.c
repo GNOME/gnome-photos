GNOME/gnome-photos

commit 470c05cd5349937e793467c26af27906a4ed43e3

preview-nav-buttons: Don't obstruct LoadMoreButton
When fading out, it is not enough to just unreveal the widgets. We
should actually hide the GtkRevealer too, so that it doesn't obstruct
others by staying in the GtkOverlay. eg., if we don't hide the one
used for the toolbar, it will block the LoadMoreButton.

src/photos-preview-nav-buttons.c
@@ -87,8 +87,22 @@ photos_preview_nav_buttons_fade_in_button (PhotosPreviewNavButtons *self, GtkWid
   if (priv->model == NULL || priv->current_path == NULL)
     return;
 
-
+  gtk_widget_show_all (widget);
   gtk_revealer_set_reveal_child (GTK_REVEALER (widget), TRUE);
+}
+
+
+static void
+photos_preview_nav_buttons_notify_child_revealed (PhotosPreviewNavButtons *self,
+                                                  GParamSpec *pspec,
+                                                  gpointer user_data)
+{
+  GtkWidget *widget = GTK_WIDGET (user_data);
+
+  if (!gtk_revealer_get_child_revealed (GTK_REVEALER (widget)))
+      gtk_widget_hide (widget);
+
+  g_signal_handlers_disconnect_by_func (widget, photos_preview_nav_buttons_notify_child_revealed, self);
 }
 
 
@@ -96,6 +110,10 @@ static void
 photos_preview_nav_buttons_fade_out_button (PhotosPreviewNavButtons *self, GtkWidget *widget)
 {
   gtk_revealer_set_reveal_child (GTK_REVEALER (widget), FALSE);
+  g_signal_connect_swapped (widget,
+                            "notify::child-revealed",
+                            G_CALLBACK (photos_preview_nav_buttons_notify_child_revealed),
+                            self);
 }
 
 
@@ -436,10 +454,6 @@ photos_preview_nav_buttons_constructed (GObject *object)
                             "motion-notify-event",
                             G_CALLBACK (photos_preview_nav_buttons_motion_notify),
                             self);
-
-  gtk_widget_show_all (priv->prev_widget);
-  gtk_widget_show_all (priv->next_widget);
-  gtk_widget_show_all (priv->toolbar_widget);
 }
 
 

commit 043cc61a807a0a4290c98d6ed35834be43a48a60

preview-nav-buttons: Hide the controls even if they didn't animate
There is no guarantee that the GtkRevealer will asynchronously animate
before emitting notify::child-revealed. There is no known breakage,
since there is no LoadMoreButton anymore. This is a pre-emptive fix.

Fallout from 470c05cd5349937e793467c26af27906a4ed43e3

https://bugzilla.gnome.org/show_bug.cgi?id=785376

src/photos-preview-nav-buttons.c
@@ -145,11 +145,11 @@ photos_preview_nav_buttons_notify_child_revealed (PhotosPreviewNavButtons *self,
 static void
 photos_preview_nav_buttons_fade_out_button (PhotosPreviewNavButtons *self, GtkWidget *widget)
 {
-  gtk_revealer_set_reveal_child (GTK_REVEALER (widget), FALSE);
   g_signal_connect_swapped (widget,
                             "notify::child-revealed",
                             G_CALLBACK (photos_preview_nav_buttons_notify_child_revealed),
                             self);
+  gtk_revealer_set_reveal_child (GTK_REVEALER (widget), FALSE);
 }
 
 