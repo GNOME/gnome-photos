GNOME/gnome-photos

commit 73e324730efcf047119ed530e1d0a7dc6a0102b7

spinner-box: Show it only when revealed (and running)
We need to hide the GtkRevealer when the child is not revealed to
ensure that the GdkWindow also goes away. Otherwise, it stays overlaid
and interferes with the crop tool. For example, the crop tool changes
the GdkCursor depending on where the mouse is relative to the crop
area. If the SpinnerBox's GdkWindow is in the overlay then the mouse
pointer changes while passing over the invisible spinner.

src/photos-spinner-box.c
@@ -47,6 +47,16 @@ G_DEFINE_TYPE (PhotosSpinnerBox, photos_spinner_box, GTK_TYPE_REVEALER);
 
 
 static void
+photos_spinner_box_notify_child_revealed (GtkRevealer *revealer)
+{
+  if (!gtk_revealer_get_child_revealed (revealer))
+    gtk_widget_hide (GTK_WIDGET (revealer));
+
+  g_signal_handlers_disconnect_by_func (revealer, photos_spinner_box_notify_child_revealed, NULL);
+}
+
+
+static void
 photos_spinner_box_constructed (GObject *object)
 {
   PhotosSpinnerBox *self = PHOTOS_SPINNER_BOX (object);
@@ -58,8 +68,6 @@ photos_spinner_box_constructed (GObject *object)
   gtk_widget_set_halign (self->spinner, GTK_ALIGN_CENTER);
   gtk_widget_set_valign (self->spinner, GTK_ALIGN_CENTER);
   gtk_container_add (GTK_CONTAINER (self), self->spinner);
-
-  gtk_widget_show_all (GTK_WIDGET (self));
 }
 
 
@@ -92,6 +100,7 @@ photos_spinner_box_new (void)
 void
 photos_spinner_box_start (PhotosSpinnerBox *self)
 {
+  gtk_widget_show_all (GTK_WIDGET (self));
   gtk_revealer_set_reveal_child (GTK_REVEALER (self), TRUE);
   gtk_spinner_start (GTK_SPINNER (self->spinner));
 }
@@ -101,5 +110,7 @@ void
 photos_spinner_box_stop (PhotosSpinnerBox *self)
 {
   gtk_revealer_set_reveal_child (GTK_REVEALER (self), FALSE);
+  g_signal_connect (self, "notify::child-revealed", G_CALLBACK (photos_spinner_box_notify_child_revealed), NULL);
+
   gtk_spinner_stop (GTK_SPINNER (self->spinner));
 }

commit 75823dd50d7ae0dfa5ed4b47a94b5ce4eba45727

spinner-box: Hide it when stopped even if it didn't animate
There is no guarantee that the GtkRevealer will asynchronously animate
before emitting notify::child-revealed. When it doesn't, the SpinnerBox
remains visible, and obstructs the hyperlink in EmptyResultsBox from
receiving the mouse event, or interferes with ToolCrop's GdkCursor.

Fallout from 73e324730efcf047119ed530e1d0a7dc6a0102b7

https://bugzilla.gnome.org/show_bug.cgi?id=785376

src/photos-spinner-box.c
@@ -104,8 +104,8 @@ photos_spinner_box_start (PhotosSpinnerBox *self)
 void
 photos_spinner_box_stop (PhotosSpinnerBox *self)
 {
-  gtk_revealer_set_reveal_child (GTK_REVEALER (self), FALSE);
   g_signal_connect (self, "notify::child-revealed", G_CALLBACK (photos_spinner_box_notify_child_revealed), NULL);
+  gtk_revealer_set_reveal_child (GTK_REVEALER (self), FALSE);
 
   gtk_spinner_stop (GTK_SPINNER (self->spinner));
 }