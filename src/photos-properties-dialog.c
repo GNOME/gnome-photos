GNOME/gnome-photos

commit d274adf0d7ff95cb1e70bfc317313168f59b46fc

properties-dialog: Use ngettext for plural forms
https://gitlab.gnome.org/GNOME/gnome-photos/issues/88

src/photos-properties-dialog.c
@@ -652,7 +652,11 @@ photos_properties_dialog_constructed (GObject *object)
       GtkWidget *dims_data;
       g_autofree gchar *dims_str = NULL;
 
-      dims_str = g_strdup_printf ("%" G_GINT64_FORMAT " × %" G_GINT64_FORMAT " pixels", width, height);
+      dims_str = g_strdup_printf (ngettext ("%" G_GINT64_FORMAT " × %" G_GINT64_FORMAT " pixel",
+                                            "%" G_GINT64_FORMAT " × %" G_GINT64_FORMAT " pixels",
+                                            height),
+                                  width,
+                                  height);
       dims_data = gtk_label_new (dims_str);
       gtk_widget_set_halign (dims_data, GTK_ALIGN_START);
       gtk_grid_attach_next_to (GTK_GRID (self->grid), dims_data, dimensions_w, GTK_POS_RIGHT, 2, 1);

commit 0a6251d2e6e2daa5cb855612f8887b7cea7856e7

properties-dialog: Pass an unsigned long to ngettext
Fallout from d274adf0d7ff95cb1e70bfc317313168f59b46fc

https://gitlab.gnome.org/GNOME/gnome-photos/issues/88

src/photos-properties-dialog.c
@@ -651,8 +651,9 @@ photos_properties_dialog_constructed (GObject *object)
     {
       GtkWidget *dims_data;
       g_autofree gchar *dims_str = NULL;
+      gulong n = (gulong) height;
 
-      dims_str = g_strdup_printf (ngettext ("%ld × %ld pixel", "%ld × %ld pixels", height), width, height);
+      dims_str = g_strdup_printf (ngettext ("%ld × %ld pixel", "%ld × %ld pixels", n), width, height);
       dims_data = gtk_label_new (dims_str);
       gtk_widget_set_halign (dims_data, GTK_ALIGN_START);
       gtk_grid_attach_next_to (GTK_GRID (self->grid), dims_data, dimensions_w, GTK_POS_RIGHT, 2, 1);