GNOME/gnome-photos

commit 4a7e2c77221b421409ad7c68e4cdccdef324d8b3

export-notification: Unbreak the "Empty Trash" button on GNOME >= 3.26
The /org/gnome/SettingsDaemon/Housekeeping D-Bus object got moved to
the org.gnome.SettingsDaemon.Housekeeping bus name when the
gnome-settings-daemon plugins were split into their own processes in
GNOME 3.26.

Support for older GNOME versions have been retained to allow new
Flatpaks to run on older host OSes.

src/photos-export-notification.c
@@ -105,6 +105,79 @@ photos_export_notification_close (PhotosExportNotification *self)
 
 
 static void
+photos_export_notification_empty_trash_call_empty_trash (GObject *source_object, GAsyncResult *res, gpointer user_data)
+{
+  GDBusConnection *connection = G_DBUS_CONNECTION (source_object);
+  GError *error = NULL;
+  GVariant *variant = NULL;
+
+  variant = g_dbus_connection_call_finish (connection, res, &error);
+  if (error != NULL)
+    {
+      if (g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN)
+          || g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_INTERFACE)
+          || g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD)
+          || g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_OBJECT))
+        {
+          g_warning ("Unable to call org.gnome.SettingsDaemon.Housekeeping:EmptyTrash: "
+                     "unknown interface/method/object/service");
+        }
+      else
+        {
+          g_warning ("Unable to call org.gnome.SettingsDaemon.Housekeeping:EmptyTrash: %s", error->message);
+        }
+
+      g_error_free (error);
+    }
+
+  g_clear_pointer (&variant, (GDestroyNotify) g_variant_unref);
+}
+
+
+static void
+photos_export_notification_empty_trash_call_empty_trash_pre_3_26 (GObject *source_object,
+                                                                  GAsyncResult *res,
+                                                                  gpointer user_data)
+{
+  GDBusConnection *connection = G_DBUS_CONNECTION (source_object);
+  GError *error = NULL;
+  GVariant *variant = NULL;
+
+  variant = g_dbus_connection_call_finish (connection, res, &error);
+  if (error != NULL)
+    {
+      if (g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_SERVICE_UNKNOWN)
+          || g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_INTERFACE)
+          || g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD)
+          || g_error_matches (error, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_OBJECT))
+        {
+          g_dbus_connection_call (connection,
+                                  "org.gnome.SettingsDaemon.Housekeeping",
+                                  "/org/gnome/SettingsDaemon/Housekeeping",
+                                  "org.gnome.SettingsDaemon.Housekeeping",
+                                  "EmptyTrash",
+                                  NULL,
+                                  NULL,
+                                  G_DBUS_CALL_FLAGS_NONE,
+                                  -1,
+                                  NULL,
+                                  photos_export_notification_empty_trash_call_empty_trash,
+                                  NULL);
+        }
+      else
+        {
+          g_warning ("Unable to call org.gnome.SettingsDaemon.Housekeeping:EmptyTrash (GNOME < 3.26): %s",
+                     error->message);
+        }
+
+      g_error_free (error);
+    }
+
+  g_clear_pointer (&variant, (GDestroyNotify) g_variant_unref);
+}
+
+
+static void
 photos_export_notification_empty_trash_bus_get (GObject *source_object, GAsyncResult *result, gpointer user_data)
 {
   PhotosExportNotification *self = PHOTOS_EXPORT_NOTIFICATION (user_data);
@@ -129,7 +202,7 @@ photos_export_notification_empty_trash_bus_get (GObject *source_object, GAsyncRe
                           G_DBUS_CALL_FLAGS_NONE,
                           -1,
                           NULL,
-                          NULL,
+                          photos_export_notification_empty_trash_call_empty_trash_pre_3_26,
                           NULL);
 
  out:

commit d7bd2486aa42682a8c07e830d2813ad5a51aa690

export-notification: Fix typo
The housekeeping plugin's D-Bus name changed in GNOME 3.24, not 3.26,
which was shipped in Fedora 26. Confusing!

Fallout from 4a7e2c77221b421409ad7c68e4cdccdef324d8b3

src/photos-export-notification.c
@@ -135,7 +135,7 @@ photos_export_notification_empty_trash_call_empty_trash (GObject *source_object,
 
 
 static void
-photos_export_notification_empty_trash_call_empty_trash_pre_3_26 (GObject *source_object,
+photos_export_notification_empty_trash_call_empty_trash_pre_3_24 (GObject *source_object,
                                                                   GAsyncResult *res,
                                                                   gpointer user_data)
 {
@@ -166,7 +166,7 @@ photos_export_notification_empty_trash_call_empty_trash_pre_3_26 (GObject *sourc
         }
       else
         {
-          g_warning ("Unable to call org.gnome.SettingsDaemon.Housekeeping:EmptyTrash (GNOME < 3.26): %s",
+          g_warning ("Unable to call org.gnome.SettingsDaemon.Housekeeping:EmptyTrash (GNOME < 3.24): %s",
                      error->message);
         }
 
@@ -202,7 +202,7 @@ photos_export_notification_empty_trash_bus_get (GObject *source_object, GAsyncRe
                           G_DBUS_CALL_FLAGS_NONE,
                           -1,
                           NULL,
-                          photos_export_notification_empty_trash_call_empty_trash_pre_3_26,
+                          photos_export_notification_empty_trash_call_empty_trash_pre_3_24,
                           NULL);
 
  out: