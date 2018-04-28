1540d2bc8843464c47690a282c1bce121ed0ff4f
Debarshi Ray <debarshir@gnome.org>
2018-04-29 00:20:00 +0200

    Add PhotosGeglBufferCodec
    https://gitlab.gnome.org/GNOME/gnome-photos/issues/63

--- src/photos-gegl.c
+++ src/photos-gegl.c
@@ -25,6 +25,7 @@
 
 #include "photos-debug.h"
 #include "photos-gegl.h"
+#include "photos-gegl-buffer-codec.h"
 #include "photos-operation-insta-clarendon.h"
 #include "photos-operation-insta-curve.h"
 #include "photos-operation-insta-filter.h"
@@ -630,6 +631,8 @@ photos_gegl_ensure_builtins (void)
 {
   static gsize once_init_value = 0;
 
+  photos_gegl_ensure_extension_points ();
+
   if (g_once_init_enter (&once_init_value))
     {
       g_type_ensure (PHOTOS_TYPE_OPERATION_INSTA_CLARENDON);
@@ -642,6 +645,23 @@ photos_gegl_ensure_builtins (void)
       g_type_ensure (PHOTOS_TYPE_OPERATION_PNG_GUESS_SIZES);
       g_type_ensure (PHOTOS_TYPE_OPERATION_SATURATION);
       g_type_ensure (PHOTOS_TYPE_OPERATION_SVG_MULTIPLY);
+
+      g_once_init_leave (&once_init_value, 1);
+    }
+}
+
+
+void
+photos_gegl_ensure_extension_points (void)
+{
+  static gsize once_init_value = 0;
+
+  if (g_once_init_enter (&once_init_value))
+    {
+      GIOExtensionPoint *extension_point;
+
+      extension_point = g_io_extension_point_register (PHOTOS_GEGL_BUFFER_CODEC_EXTENSION_POINT_NAME);
+      g_io_extension_point_set_required_type (extension_point, PHOTOS_TYPE_GEGL_BUFFER_CODEC);
 
       g_once_init_leave (&once_init_value, 1);
     }
