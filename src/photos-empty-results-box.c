GNOME/gnome-photos

commit 556a9f30613525631e2495639a6e7e716340713b

flatpak, utils: Add a Flatpak-friendly method to spawn the GOA panel
The Flatpak bundle doesn't include the gnome-control-center binary,
and the sandbox prevents it from accessing one that's part of the host
operating system or a different bundle. Therefore, it's not possible
to use g_app_info_create_from_commandline to launch the Online
Accounts panel when running as a Flatpak.

However, with a hole poked in the sandbox, it will be able to see the
org.gtk.Actions-based [1] D-Bus API offered by gnome-control-center
[2], which can be conveniently accessed through GApplication's remote
GActionGroup implementation.

This will be used by the subsequent commit.

[1] https://wiki.gnome.org/Projects/GLib/GApplication/DBusAPI
[2] gnome-control-center commit ab0576f1f00172c4
    https://bugzilla.gnome.org/show_bug.cgi?id=696054

src/photos-utils.c
@@ -1,6 +1,6 @@
 /*
  * Photos - access, organize and share your photos on GNOME
- * Copyright © 2012 – 2017 Red Hat, Inc.
+ * Copyright © 2012 – 2018 Red Hat, Inc.
  * Copyright © 2009 Yorba Foundation
  *
  * This program is free software: you can redistribute it and/or modify
@@ -1076,6 +1076,39 @@ photos_utils_get_version (void)
   ret_val = PACKAGE_VERSION;
 #endif
 
+  return ret_val;
+}
+
+
+gboolean
+photos_utils_launch_online_accounts (const gchar *account_id, GError **error)
+{
+  GVariant *parameters;
+  g_autoptr (GApplication) control_center = NULL;
+  g_auto (GVariantBuilder) panel_parameters = G_VARIANT_BUILDER_INIT (G_VARIANT_TYPE ("av"));
+  gboolean ret_val = FALSE;
+  const gchar *control_center_id = "org.gnome.ControlCenter";
+
+  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
+
+  control_center = g_application_new (control_center_id, G_APPLICATION_IS_LAUNCHER);
+  if (!g_application_register (control_center, NULL, error))
+    goto out;
+
+  if (account_id != NULL && account_id[0] != '\0')
+    {
+      GVariant *account_id_variant;
+
+      account_id_variant = g_variant_new_string (account_id);
+      g_variant_builder_add (&panel_parameters, "v", account_id_variant);
+    }
+
+  parameters = g_variant_new ("(s@av)", "online-accounts", g_variant_builder_end (&panel_parameters));
+  g_action_group_activate_action (G_ACTION_GROUP (control_center), "launch-panel", parameters);
+
+  ret_val = TRUE;
+
+ out:
   return ret_val;
 }
 

src/photos-utils.h
@@ -1,6 +1,6 @@
 /*
  * Photos - access, organize and share your photos on GNOME
- * Copyright © 2012 – 2017 Red Hat, Inc.
+ * Copyright © 2012 – 2018 Red Hat, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -145,6 +145,8 @@ gboolean         photos_utils_get_selection_mode          (void);
 GList           *photos_utils_get_urns_from_items         (GList *items);
 
 const gchar     *photos_utils_get_version                 (void);
+
+gboolean         photos_utils_launch_online_accounts      (const gchar *account_id, GError **error);
 
 void             photos_utils_list_box_header_func        (GtkListBoxRow *row,
                                                            GtkListBoxRow *before,

commit 924fb4ec900512da0943cbc549f61b3bd3c34dcd

Don't freeze the UI when launching Settings -> Online Accounts
The UI freezes for a moment when launching Settings -> Online Accounts
because g_application_register does slow I/O. After creating the
GApplication's GDBusActionGroup, it queries the list of remote actions
from the primary application instance over D-Bus to populate it.
Fortunately, it's not necessary to fill in the GDBusActionGroup for
activating a GAction. Therefore, this can be solved by directly using
a GDBusActionGroup instead of going via a launcher GApplication.

This does lose the platform data that the launcher GApplication would
have passed when invoking the remote GAction, but hopefully that
won't prove crucial.

The loss of error handling doesn't matter. g_application_register was
the only fallible step, and it could have only failed while querying
the list of actions from the primary, something that's not needed
anyway. If anything, it simplifies the code.

Fallout from 556a9f30613525631e2495639a6e7e716340713b

src/photos-empty-results-box.c
@@ -55,16 +55,7 @@ photos_empty_results_box_activate_link (PhotosEmptyResultsBox *self, const gchar
   if (g_strcmp0 (uri, "system-settings") != 0)
     goto out;
 
-  {
-    g_autoptr (GError) error = NULL;
-
-    if (!photos_utils_launch_online_accounts (NULL, &error))
-      {
-        g_warning ("Unable to open Online Accounts panel: %s", error->message);
-        goto out;
-      }
-  }
-
+  photos_utils_launch_online_accounts (NULL);
   ret_val = TRUE;
 
  out:

src/photos-source-notification.c
@@ -94,19 +94,7 @@ photos_source_notification_settings_clicked (PhotosSourceNotification *self)
 
   account = goa_object_peek_account (object);
   id = goa_account_get_id (account);
-
-  {
-    g_autoptr (GError) error = NULL;
-
-    if (!photos_utils_launch_online_accounts (id, &error))
-      {
-        g_warning ("Unable to open Online Accounts panel: %s", error->message);
-        goto out;
-      }
-  }
-
- out:
-  return;
+  photos_utils_launch_online_accounts (id);
 }
 
 

src/photos-utils.c
@@ -1077,19 +1077,18 @@ photos_utils_get_version (void)
 }
 
 
-gboolean
-photos_utils_launch_online_accounts (const gchar *account_id, GError **error)
+void
+photos_utils_launch_online_accounts (const gchar *account_id)
 {
-  g_autoptr (GApplication) control_center = NULL;
+  GApplication *app;
+  g_autoptr (GDBusActionGroup) control_center = NULL;
+  GDBusConnection *connection;
   GVariant *parameters;
   g_auto (GVariantBuilder) panel_parameters = G_VARIANT_BUILDER_INIT (G_VARIANT_TYPE ("av"));
-  gboolean ret_val = FALSE;
 
-  g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
-
-  control_center = g_application_new ("org.gnome.ControlCenter", G_APPLICATION_IS_LAUNCHER);
-  if (!g_application_register (control_center, NULL, error))
-    goto out;
+  app = g_application_get_default ();
+  connection = g_application_get_dbus_connection (app);
+  control_center = g_dbus_action_group_get (connection, "org.gnome.ControlCenter", "/org/gnome/ControlCenter");
 
   if (account_id != NULL && account_id[0] != '\0')
     {
@@ -1101,11 +1100,6 @@ photos_utils_launch_online_accounts (const gchar *account_id, GError **error)
 
   parameters = g_variant_new ("(s@av)", "online-accounts", g_variant_builder_end (&panel_parameters));
   g_action_group_activate_action (G_ACTION_GROUP (control_center), "launch-panel", parameters);
-
-  ret_val = TRUE;
-
- out:
-  return ret_val;
 }
 
 

src/photos-utils.h
@@ -146,7 +146,7 @@ GList           *photos_utils_get_urns_from_items         (GList *items);
 
 const gchar     *photos_utils_get_version                 (void);
 
-gboolean         photos_utils_launch_online_accounts      (const gchar *account_id, GError **error);
+void             photos_utils_launch_online_accounts      (const gchar *account_id);
 
 void             photos_utils_list_box_header_func        (GtkListBoxRow *row,
                                                            GtkListBoxRow *before,