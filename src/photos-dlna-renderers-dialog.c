GNOME/gnome-photos

commit 8e0c3cc17c4562420ff306600b7845af4f917b59

dlna: Display item on remote DLNA renderer using dLeyna
Let the user choose a DLNA renderer device from the list of DMRs
discovered on the network and display the current item there.

This adds a soft-dependency on the deyna-renderer DBus service: if the
service is not activatable the functionality will be automatically
disabled.

https://bugzilla.gnome.org/show_bug.cgi?id=701775

src/photos-dlna-renderers-dialog.c
@@ -1,0 +1,290 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2013 Intel Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "config.h"
+
+#include "photos-dlna-renderers-dialog.h"
+
+#include <gio/gio.h>
+#include <glib.h>
+#include <glib/gi18n.h>
+
+#include "photos-dleyna-renderer-device.h"
+#include "photos-dleyna-renderer-push-host.h"
+#include "photos-dlna-renderer.h"
+#include "photos-dlna-renderers-manager.h"
+#include "photos-item-manager.h"
+#include "photos-local-item.h"
+#include "photos-mode-controller.h"
+#include "photos-remote-display-manager.h"
+
+
+struct _PhotosDlnaRenderersDialogPrivate
+{
+  PhotosBaseManager *item_mngr;
+  PhotosDlnaRenderersManager *renderers_mngr;
+  PhotosRemoteDisplayManager *remote_mngr;
+  PhotosModeController *mode_cntrlr;
+  GtkListBox *listbox;
+  gchar *urn;
+};
+
+
+enum
+{
+  PROP_0,
+  PROP_URN
+};
+
+
+G_DEFINE_TYPE (PhotosDlnaRenderersDialog, photos_dlna_renderers_dialog, GTK_TYPE_DIALOG);
+
+
+static void
+photos_dlna_renderers_separator_cb (GtkListBoxRow *row,
+                                    GtkListBoxRow *before,
+                                    gpointer user_data)
+{
+  GtkWidget *separator;
+
+  separator = gtk_list_box_row_get_header (row);
+  if (separator == NULL && before != NULL)
+    {
+      separator = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
+      gtk_list_box_row_set_header (row, separator);
+      gtk_widget_show (separator);
+    }
+}
+
+
+static void
+photos_dlna_renderers_dialog_row_activated_cb (PhotosDlnaRenderersDialog *self,
+                                               GtkListBoxRow             *row,
+                                               gpointer                   user_data)
+{
+  PhotosDlnaRenderersDialogPrivate *priv = self->priv;
+  PhotosBaseItem *item;
+  PhotosDlnaRenderer *renderer;
+
+  renderer = g_object_get_data (G_OBJECT (row), "renderer");
+  item = PHOTOS_BASE_ITEM (photos_base_manager_get_object_by_id (priv->item_mngr,
+                                                                 priv->urn));
+  photos_remote_display_manager_set_renderer (priv->remote_mngr, renderer);
+  photos_remote_display_manager_render (priv->remote_mngr, item);
+
+  photos_mode_controller_set_window_mode (priv->mode_cntrlr, PHOTOS_WINDOW_MODE_OVERVIEW);
+
+  gtk_dialog_response (GTK_DIALOG (self), GTK_RESPONSE_ACCEPT);
+}
+
+
+static void
+photos_dlna_renderers_dialog_dispose (GObject *object)
+{
+  PhotosDlnaRenderersDialog *self = PHOTOS_DLNA_RENDERERS_DIALOG (object);
+  PhotosDlnaRenderersDialogPrivate *priv = self->priv;
+
+  g_clear_object (&priv->item_mngr);
+  g_clear_object (&priv->renderers_mngr);
+  g_clear_object (&priv->remote_mngr);
+  g_clear_object (&priv->mode_cntrlr);
+
+  G_OBJECT_CLASS (photos_dlna_renderers_dialog_parent_class)->dispose (object);
+}
+
+
+static void
+photos_dlna_renderers_dialog_finalize (GObject *object)
+{
+  PhotosDlnaRenderersDialog *self = PHOTOS_DLNA_RENDERERS_DIALOG (object);
+  PhotosDlnaRenderersDialogPrivate *priv = self->priv;
+
+  g_free (priv->urn);
+
+  G_OBJECT_CLASS (photos_dlna_renderers_dialog_parent_class)->finalize (object);
+}
+
+
+static void
+photos_dlna_renderers_dialog_set_property (GObject      *object,
+                                           guint         prop_id,
+                                           const GValue *value,
+                                           GParamSpec   *pspec)
+{
+  PhotosDlnaRenderersDialog *self = PHOTOS_DLNA_RENDERERS_DIALOG (object);
+
+  switch (prop_id)
+    {
+    case PROP_URN:
+      self->priv->urn = g_value_dup_string (value);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+
+static void
+photos_dlna_renderers_dialog_set_icon_cb (GObject      *source_object,
+                                          GAsyncResult *res,
+                                          gpointer      user_data)
+{
+  GdkPixbuf *pixbuf;
+  GtkImage *image = GTK_IMAGE (user_data);
+  PhotosDlnaRenderer *renderer = PHOTOS_DLNA_RENDERER (source_object);
+  GError *error = NULL;
+
+  pixbuf = photos_dlna_renderer_get_icon_finish (renderer, res, &error);
+  if (error != NULL)
+    {
+      g_warning ("Unable to load renderer icon: %s", error->message);
+      g_error_free (error);
+      goto out;
+    }
+
+  gtk_image_set_from_pixbuf (image, pixbuf);
+
+out:
+  /* release the ref we took before the async call */
+  g_object_unref (image);
+}
+
+
+static void
+photos_dlna_renderers_dialog_add_renderer (PhotosDlnaRenderersDialog *self,
+                                           PhotosDlnaRenderer *renderer)
+{
+  PhotosDlnaRenderersDialogPrivate *priv = self->priv;
+  GIcon *icon;
+  GtkWidget *row;
+  GtkWidget *row_grid;
+  GtkWidget *image;
+  GtkWidget *label;
+  const gchar *name;
+  const gchar *icon_url;
+
+  row = gtk_list_box_row_new ();
+  row_grid = gtk_grid_new ();
+  gtk_container_set_border_width (GTK_CONTAINER (row_grid), 12);
+  gtk_orientable_set_orientation (GTK_ORIENTABLE (row_grid), GTK_ORIENTATION_HORIZONTAL);
+  gtk_grid_set_column_spacing (GTK_GRID (row_grid), 12);
+  gtk_container_add (GTK_CONTAINER (row), row_grid);
+  gtk_container_add (GTK_CONTAINER (priv->listbox), row);
+
+  g_object_set_data_full (G_OBJECT (row), "renderer", renderer, g_object_unref);
+
+  name = photos_dlna_renderer_get_friendly_name (renderer);
+
+  icon = g_themed_icon_new_with_default_fallbacks ("video-display-symbolic");
+  image = gtk_image_new_from_gicon (icon, GTK_ICON_SIZE_DIALOG);
+
+  g_object_ref (image); /* keep a ref for the following async call and release it in the callback */
+  photos_dlna_renderer_get_icon (renderer, "", "", NULL,
+                                 photos_dlna_renderers_dialog_set_icon_cb, image);
+
+  gtk_container_add (GTK_CONTAINER (row_grid), image);
+
+  label = gtk_label_new (NULL);
+  gtk_label_set_text (GTK_LABEL (label), name);
+  gtk_container_add (GTK_CONTAINER (row_grid), label);
+}
+
+
+void
+photos_dlna_renderers_dialog_response_cb (GtkDialog *dialog,
+                                          gint       response_id,
+                                          gpointer   user_data)
+{
+  gtk_widget_destroy (GTK_WIDGET (dialog));
+}
+
+
+static void
+photos_dlna_renderers_dialog_init (PhotosDlnaRenderersDialog *self)
+{
+  PhotosDlnaRenderersDialogPrivate *priv;
+  GList *renderers;
+
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+                                            PHOTOS_TYPE_DLNA_RENDERERS_DIALOG,
+                                            PhotosDlnaRenderersDialogPrivate);
+  priv = self->priv;
+  priv->item_mngr = photos_item_manager_new ();
+  priv->renderers_mngr = photos_dlna_renderers_manager_dup_singleton ();
+  priv->remote_mngr = photos_remote_display_manager_dup_singleton ();
+  priv->mode_cntrlr = photos_mode_controller_new ();
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  gtk_list_box_set_header_func (priv->listbox, photos_dlna_renderers_separator_cb, NULL, NULL);
+
+  renderers = photos_dlna_renderers_manager_dup_renderers (priv->renderers_mngr);
+
+  while (renderers != NULL)
+    {
+      PhotosDlnaRenderer *renderer = PHOTOS_DLNA_RENDERER (renderers->data);
+
+      photos_dlna_renderers_dialog_add_renderer (self, renderer);
+      renderers = g_list_delete_link (renderers, renderers);
+    }
+
+  g_signal_connect (self, "response", G_CALLBACK (photos_dlna_renderers_dialog_response_cb), NULL);
+}
+
+
+static void
+photos_dlna_renderers_dialog_class_init (PhotosDlnaRenderersDialogClass *class)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (class);
+
+  object_class->dispose = photos_dlna_renderers_dialog_dispose;
+  object_class->finalize = photos_dlna_renderers_dialog_finalize;
+  object_class->set_property = photos_dlna_renderers_dialog_set_property;
+
+  g_object_class_install_property (object_class,
+                                   PROP_URN,
+                                   g_param_spec_string ("urn",
+                                                        "Uniform Resource Name",
+                                                        "An unique ID associated with this item",
+                                                        NULL,
+                                                        G_PARAM_CONSTRUCT_ONLY | G_PARAM_WRITABLE));
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/org/gnome/photos/dlna-renderers-dialog.ui");
+
+  gtk_widget_class_bind_child (widget_class, PhotosDlnaRenderersDialogPrivate, listbox);
+  gtk_widget_class_bind_callback (widget_class, photos_dlna_renderers_dialog_row_activated_cb);
+
+  g_type_class_add_private (class, sizeof (PhotosDlnaRenderersDialogPrivate));
+}
+
+
+GtkWidget *
+photos_dlna_renderers_dialog_new (GtkWindow *parent,
+                                  const gchar *urn)
+{
+  g_return_val_if_fail (GTK_IS_WINDOW (parent), NULL);
+
+  return g_object_new (PHOTOS_TYPE_DLNA_RENDERERS_DIALOG,
+                       "transient-for", parent, "urn", urn, NULL);
+}

configure.ac
@@ -26,7 +26,7 @@ GLIB_GSETTINGS
 
 EXEMPI_MIN_VERSION=1.99.5
 GLIB_MIN_VERSION=2.35.1
-GTK_MIN_VERSION=3.8.0
+GTK_MIN_VERSION=3.9.4
 LIBEXIF_MIN_VERSION=0.6.14
 LIBRSVG_MIN_VERSION=2.26.0
 

po/POTFILES.in
@@ -8,6 +8,7 @@ src/eog-uri-converter.c
 src/eog-util.c
 src/photos-application.c
 [type: gettext/glade]src/photos-app-menu.ui
+[type: gettext/glade]src/photos-dlna-renderers-dialog.ui
 src/photos-embed.c
 src/photos-empty-results-box.c
 src/photos-indexing-notification.c

src/Makefile.am
@@ -8,8 +8,16 @@ gnome_photos_built_sources = \
 	photos-about-data.h \
 	photos-enums.c \
 	photos-enums.h \
+	photos-dleyna-renderer-device.c \
+	photos-dleyna-renderer-device.h \
+	photos-dleyna-renderer-manager.c \
+	photos-dleyna-renderer-manager.h \
+	photos-dleyna-renderer-push-host.c \
+	photos-dleyna-renderer-push-host.h \
 	photos-marshalers.c \
 	photos-marshalers.h \
+	photos-mpris-player.c \
+	photos-mpris-player.h \
 	photos-resources.c \
 	photos-resources.h \
 	photos-tracker-resources.c \
@@ -66,6 +74,12 @@ gnome_photos_SOURCES = \
 	photos-create-collection-job.h \
 	photos-delete-item-job.c \
 	photos-delete-item-job.h \
+	photos-dlna-renderer.c \
+	photos-dlna-renderer.h \
+	photos-dlna-renderers-dialog.c \
+	photos-dlna-renderers-dialog.h \
+	photos-dlna-renderers-manager.c \
+	photos-dlna-renderers-manager.h \
 	photos-embed.c \
 	photos-embed.h \
 	photos-empty-results-box.c \
@@ -124,6 +138,8 @@ gnome_photos_SOURCES = \
 	photos-query.h \
 	photos-query-builder.h \
 	photos-query-builder.c \
+	photos-remote-display-manager.c \
+	photos-remote-display-manager.h \
 	photos-search-type.c \
 	photos-search-type.h \
 	photos-search-type-manager.c \
@@ -180,10 +196,15 @@ EXTRA_DIST = \
 	photos-generate-about \
 	photos.gresource.xml \
 	photos-app-menu.ui \
+	photos-dlna-renderers-dialog.ui \
 	photos-marshalers.list \
 	photos-preview-menu.ui \
 	photos-selection-menu.ui \
 	photos-tracker-resources.xml \
+	photos-dleyna-renderer-device.xml \
+	photos-dleyna-renderer-manager.xml \
+	photos-dleyna-renderer-push-host.xml \
+	photos-mpris-player.xml \
 	$(null)
 
 AM_CPPFLAGS = \
@@ -361,6 +382,35 @@ photos-resources.h: photos.gresource.xml $(shell $(GLIB_COMPILE_RESOURCES) --sou
 	$(AM_V_GEN)$(GLIB_COMPILE_RESOURCES) --target=$@ \
 		--sourcedir=$(srcdir) \
 		--generate-header \
+		$<
+
+photos-dleyna-renderer-device.h photos-dleyna-renderer-device.c: photos-dleyna-renderer-device.xml
+	$(AM_V_GEN)gdbus-codegen \
+		--c-namespace Dleyna \
+		--generate-c-code photos-dleyna-renderer-device \
+		--interface-prefix com.intel.dLeynaRenderer. \
+		--annotate 'com.intel.dLeynaRenderer.RendererDevice.GetIcon()[Bytes]' org.gtk.GDBus.C.ForceGVariant true \
+		$<
+
+photos-dleyna-renderer-manager.h photos-dleyna-renderer-manager.c: photos-dleyna-renderer-manager.xml
+	$(AM_V_GEN)gdbus-codegen \
+		--c-namespace Dleyna \
+		--generate-c-code photos-dleyna-renderer-manager \
+		--interface-prefix com.intel.dLeynaRenderer. \
+		$<
+
+photos-dleyna-renderer-push-host.h photos-dleyna-renderer-push-host.c: photos-dleyna-renderer-push-host.xml
+	$(AM_V_GEN)gdbus-codegen \
+		--c-namespace Dleyna \
+		--generate-c-code photos-dleyna-renderer-push-host \
+		--interface-prefix com.intel.dLeynaRenderer. \
+		$<
+
+photos-mpris-player.h photos-mpris-player.c: photos-mpris-player.xml
+	$(AM_V_GEN)gdbus-codegen \
+		--c-namespace Mpris \
+		--generate-c-code photos-mpris-player \
+		--interface-prefix org.mpris.MediaPlayer2. \
 		$<
 
 photos-tracker-resources.h photos-tracker-resources.c: photos-tracker-resources.xml

src/photos-application.c
@@ -33,6 +33,7 @@
 
 #include "eog-debug.h"
 #include "photos-application.h"
+#include "photos-dlna-renderers-dialog.h"
 #include "photos-item-manager.h"
 #include "photos-main-window.h"
 #include "photos-mode-controller.h"
@@ -52,6 +53,7 @@ struct _PhotosApplicationPrivate
   GSimpleAction *sel_all_action;
   GSimpleAction *sel_none_action;
   GSimpleAction *set_bg_action;
+  GSimpleAction *remote_display_action;
   GtkWidget *main_window;
   PhotosBaseManager *item_mngr;
   PhotosModeController *mode_cntrlr;
@@ -147,6 +149,24 @@ photos_application_properties (PhotosApplication *self)
   dialog = photos_properties_dialog_new (GTK_WINDOW (priv->main_window), id);
   gtk_widget_show_all (dialog);
   g_signal_connect (dialog, "response", G_CALLBACK (gtk_widget_destroy), NULL);
+}
+
+
+static void
+photos_application_remote_display_current (PhotosApplication *self)
+{
+  PhotosApplicationPrivate *priv = self->priv;
+  PhotosBaseItem *item;
+  GtkWidget *dialog;
+  const gchar *urn;
+
+  item = PHOTOS_BASE_ITEM (photos_base_manager_get_active_object (priv->item_mngr));
+  if (item == NULL)
+    return;
+
+  urn = photos_base_item_get_id (item);
+  dialog = photos_dlna_renderers_dialog_new (GTK_WINDOW (priv->main_window), urn);
+  gtk_widget_show_all (dialog);
 }
 
 
@@ -265,6 +285,10 @@ photos_application_startup (GApplication *application)
   priv->properties_action = g_simple_action_new ("properties", NULL);
   g_signal_connect_swapped (priv->properties_action, "activate", G_CALLBACK (photos_application_properties), self);
   g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (priv->properties_action));
+
+  priv->remote_display_action = g_simple_action_new ("remote-display-current", NULL);
+  g_signal_connect_swapped (priv->remote_display_action, "activate", G_CALLBACK (photos_application_remote_display_current), self);
+  g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (priv->remote_display_action));
 
   action = g_simple_action_new ("quit", NULL);
   g_signal_connect_swapped (action, "activate", G_CALLBACK (photos_application_quit), self);

src/photos-dleyna-renderer-device.xml
@@ -1,0 +1,62 @@
+<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+                      "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<!--
+ Photos - access, organize and share your photos on GNOME
+ Copyright © 2013 Intel Corporation. All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+-->
+<!-- GDBus 2.37.1 -->
+<node>
+  <interface name="com.intel.dLeynaRenderer.RendererDevice">
+    <method name="Cancel">
+    </method>
+    <method name="GetIcon">
+      <arg type="s" name="RequestedMimeType" direction="in">
+      </arg>
+      <arg type="s" name="Resolution" direction="in">
+      </arg>
+      <arg type="ay" name="Bytes" direction="out">
+      </arg>
+      <arg type="s" name="MimeType" direction="out">
+      </arg>
+    </method>
+    <property type="s" name="DeviceType" access="read">
+    </property>
+    <property type="s" name="UDN" access="read">
+    </property>
+    <property type="s" name="FriendlyName" access="read">
+    </property>
+    <property type="s" name="IconURL" access="read">
+    </property>
+    <property type="s" name="Manufacturer" access="read">
+    </property>
+    <property type="s" name="ManufacturerUrl" access="read">
+    </property>
+    <property type="s" name="ModelDescription" access="read">
+    </property>
+    <property type="s" name="ModelName" access="read">
+    </property>
+    <property type="s" name="ModelNumber" access="read">
+    </property>
+    <property type="s" name="SerialNumber" access="read">
+    </property>
+    <property type="s" name="PresentationURL" access="read">
+    </property>
+    <property type="s" name="ProtocolInfo" access="read">
+    </property>
+  </interface>
+</node>

src/photos-dleyna-renderer-manager.xml
@@ -1,0 +1,46 @@
+<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+                      "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<!--
+ Photos - access, organize and share your photos on GNOME
+ Copyright © 2013 Intel Corporation. All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+-->
+<!-- GDBus 2.37.1 -->
+<node>
+  <interface name="com.intel.dLeynaRenderer.Manager">
+    <method name="GetVersion">
+      <arg type="s" name="Version" direction="out">
+      </arg>
+    </method>
+    <method name="Release">
+    </method>
+    <method name="GetRenderers">
+      <arg type="as" name="Renderers" direction="out">
+      </arg>
+    </method>
+    <method name="Rescan">
+    </method>
+    <signal name="FoundRenderer">
+      <arg type="s" name="Path">
+      </arg>
+    </signal>
+    <signal name="LostRenderer">
+      <arg type="s" name="Path">
+      </arg>
+    </signal>
+  </interface>
+</node>

src/photos-dleyna-renderer-push-host.xml
@@ -1,0 +1,36 @@
+<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+                      "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<!--
+ Photos - access, organize and share your photos on GNOME
+ Copyright © 2013 Intel Corporation. All rights reserved.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ 02110-1301, USA.
+-->
+<!-- GDBus 2.37.1 -->
+<node>
+  <interface name="com.intel.dLeynaRenderer.PushHost">
+    <method name="HostFile">
+      <arg type="s" name="Path" direction="in">
+      </arg>
+      <arg type="s" name="Uri" direction="out">
+      </arg>
+    </method>
+    <method name="RemoveFile">
+      <arg type="s" name="Path" direction="in">
+      </arg>
+    </method>
+  </interface>
+</node>

commit 6c2f69d0df8e33bfcb846e56521b75a54a41192f

dlna-renderers-dialog: Remove redundant headers
Fallout from 8e0c3cc17c4562420ff306600b7845af4f917b59

src/photos-dlna-renderers-dialog.c
@@ -27,8 +27,6 @@
 #include <glib/gi18n.h>
 
 #include "photos-base-manager.h"
-#include "photos-dleyna-renderer-device.h"
-#include "photos-dleyna-renderer-push-host.h"
 #include "photos-dlna-renderer.h"
 #include "photos-dlna-renderers-manager.h"
 #include "photos-item-manager.h"