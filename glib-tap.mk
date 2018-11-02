519f786327a51313ce4e880e99c724f009b65f2e
Debarshi Ray <debarshir@gnome.org>
2018-11-02 12:38:55 +0100

    build: Update glib-tap.mk from GLib
    Use AM_TESTS_ENVIRONMENT rather than TESTS_ENVIRONMENT because the
    former is reserved for the user to set when running the tests; fix a
    misnamed variable; add missing mkdir for .test generation rule because
    it can’t be guaranteed that the builddir actually exists; and use TAP
    for installed tests too.
    
    However, the Type is retained as 'session-exclusive' due to the
    presence of a graphical test.

--- glib-tap.mk
+++ glib-tap.mk
@@ -1,6 +1,6 @@
 # GLIB - Library of useful C routines
 
-TESTS_ENVIRONMENT= \
+AM_TESTS_ENVIRONMENT= \
 	G_TEST_SRCDIR="$(abs_srcdir)" 		\
 	G_TEST_BUILDDIR="$(abs_builddir)" 	\
 	G_DEBUG=gc-friendly 			\
@@ -112,7 +112,7 @@ if ENABLE_INSTALLED_TESTS
 installed_test_PROGRAMS += $(test_programs) $(installed_test_programs) \
                           $(test_extra_programs) $(installed_test_extra_programs)
 installed_test_SCRIPTS += $(test_scripts) $(installed_test_scripts) \
-                          $(test_extra_scripts) $(test_installed_extra_scripts)
+                          $(test_extra_scripts) $(installed_test_extra_scripts)
 installed_test_SCRIPTS += $(dist_test_scripts) $(dist_test_extra_scripts) \
                           $(dist_installed_test_scripts) $(dist_installed_test_extra_scripts)
 nobase_installed_test_DATA += $(test_data) $(installed_test_data)
@@ -125,9 +125,11 @@ installed_testcases = $(test_programs) $(installed_test_programs) \
 installed_test_meta_DATA = $(installed_testcases:=.test)
 
 %.test: %$(EXEEXT) Makefile
-	$(AM_V_GEN) (echo '[Test]' > $@.tmp; \
+	$(AM_V_GEN) ($(MKDIR_P) $(@D); \
+	echo '[Test]' > $@.tmp; \
 	echo 'Type=session-exclusive' >> $@.tmp; \
-	echo 'Exec=$(installed_testdir)/$(notdir $<)' >> $@.tmp; \
+	echo 'Exec=$(installed_testdir)/$(notdir $<) --tap' >> $@.tmp; \
+	echo 'Output=TAP' >> $@.tmp; \
 	mv $@.tmp $@)
 
 CLEANFILES += $(installed_test_meta_DATA)
