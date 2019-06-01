6d9a9b5ce9646b1c55dbb4a5e5e338bab9329257
Debarshi Ray <debarshir@gnome.org>
2019-06-01 12:49:20 +0000

    Add PhotosGeglBufferCodecRaw
    https://gitlab.gnome.org/GNOME/gnome-photos/issues/63

---
+++ src/photos-gegl-buffer-codec-raw.h
@@ -0,0 +1,35 @@
+/*
+ * Photos - access, organize and share your photos on GNOME
+ * Copyright © 2019 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PHOTOS_GEGL_BUFFER_CODEC_RAW_H
+#define PHOTOS_GEGL_BUFFER_CODEC_RAW_H
+
+#include "photos-gegl-buffer-codec.h"
+
+G_BEGIN_DECLS
+
+#define PHOTOS_TYPE_GEGL_BUFFER_CODEC_RAW (photos_gegl_buffer_codec_raw_get_type ())
+G_DECLARE_FINAL_TYPE (PhotosGeglBufferCodecRaw,
+                      photos_gegl_buffer_codec_raw,
+                      PHOTOS,
+                      GEGL_BUFFER_CODEC_RAW,
+                      PhotosGeglBufferCodec);
+
+G_END_DECLS
+
+#endif /* PHOTOS_GEGL_BUFFER_CODEC_RAW_H */
