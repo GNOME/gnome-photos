GNOME/gnome-photos

commit 3d4f17a87a4da426c861057e989602387ece83fb

search-type-manager: Clean up
+ There is no need for a builtin "all" PhotosSearchType. We will always
  have the distinction between albums, photos and favorites.
+ Not all albums are nie:DataObjects. Only nfo:DataContainer is
  mandatory.

Fixes: https://bugzilla.gnome.org/709367

src/photos-search-type-manager.c
@@ -70,13 +70,9 @@ photos_search_type_manager_init (PhotosSearchTypeManager *self)
                                 PHOTOS_QUERY_COLLECTIONS_IDENTIFIER);
   fav_filter = g_strdup_printf ("(%s || %s)", col_filter, item_filter);
 
-  search_type = photos_search_type_new (PHOTOS_SEARCH_TYPE_STOCK_ALL, _("All"));
-  photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
-  g_object_unref (search_type);
-
   search_type = photos_search_type_new_full (PHOTOS_SEARCH_TYPE_STOCK_COLLECTIONS,
                                              _("Albums"),
-                                             "?urn a nfo:DataContainer ; a nie:DataObject",
+                                             "?urn a nfo:DataContainer",
                                              col_filter);
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
@@ -88,15 +84,14 @@ photos_search_type_manager_init (PhotosSearchTypeManager *self)
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
 
-  /* This is only meant to be used with ALL. */
   search_type = photos_search_type_new_full (PHOTOS_SEARCH_TYPE_STOCK_PHOTOS,
                                              _("Photos"),
-                                             "",
-                                             item_filter);
+                                             "?urn a nmm:Photo",
+                                             "(true)");
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
 
-  photos_base_manager_set_active_object_by_id (PHOTOS_BASE_MANAGER (self), PHOTOS_SEARCH_TYPE_STOCK_ALL);
+  photos_base_manager_set_active_object_by_id (PHOTOS_BASE_MANAGER (self), PHOTOS_SEARCH_TYPE_STOCK_PHOTOS);
 
   g_free (col_filter);
   g_free (fav_filter);
@@ -123,30 +118,7 @@ gchar *
 photos_search_type_manager_get_where (PhotosSearchTypeManager *self)
 {
   GObject *search_type;
-  gboolean is_all;
-  const gchar *all_where = "?urn a rdfs:Resource";
-  gchar *id;
-  gchar *ret_val;
 
   search_type = photos_base_manager_get_active_object (PHOTOS_BASE_MANAGER (self));
-  if (search_type == NULL)
-    {
-      ret_val = g_strdup (all_where);
-      goto out;
-    }
-
-  g_object_get (search_type, "id", &id, NULL);
-  is_all = (g_strcmp0 (id, PHOTOS_SEARCH_TYPE_STOCK_ALL) == 0);
-  g_free (id);
-
-  if (is_all)
-    {
-      ret_val = g_strdup (all_where);
-      goto out;
-    }
-
-  ret_val = photos_filterable_get_where (PHOTOS_FILTERABLE (search_type));
-
- out:
-  return ret_val;
+  return photos_filterable_get_where (PHOTOS_FILTERABLE (search_type));
 }

src/photos-search-type.h
@@ -51,7 +51,6 @@ G_BEGIN_DECLS
   (G_TYPE_INSTANCE_GET_CLASS ((obj), \
    PHOTOS_TYPE_SEARCH_TYPE, PhotosSearchTypeClass))
 
-#define PHOTOS_SEARCH_TYPE_STOCK_ALL    "all"
 #define PHOTOS_SEARCH_TYPE_STOCK_COLLECTIONS "collections"
 #define PHOTOS_SEARCH_TYPE_STOCK_FAVORITES "favorites"
 #define PHOTOS_SEARCH_TYPE_STOCK_PHOTOS "photos"

src/photos-query-builder.c
@@ -66,7 +66,7 @@ photos_query_builder_set_search_type (gint flags)
   else if (flags & PHOTOS_QUERY_FLAGS_FAVORITES)
     photos_base_manager_set_active_object_by_id (srch_typ_mngr, PHOTOS_SEARCH_TYPE_STOCK_FAVORITES);
   else
-    photos_base_manager_set_active_object_by_id (srch_typ_mngr, PHOTOS_SEARCH_TYPE_STOCK_ALL);
+    photos_base_manager_set_active_object_by_id (srch_typ_mngr, PHOTOS_SEARCH_TYPE_STOCK_PHOTOS);
 
   g_object_unref (srch_typ_mngr);
 }

commit 8c94c10642caffcc5510f99d695aef364b9cb743

search-type, search-type-manager: Simplify code
Fallout from 3d4f17a87a4da426c861057e989602387ece83fb

https://gitlab.gnome.org/GNOME/gnome-photos/-/merge_requests/165

src/photos-search-type-manager.c
@@ -117,33 +117,33 @@ photos_search_type_manager_init (PhotosSearchTypeManager *self)
                                 PHOTOS_QUERY_COLLECTIONS_IDENTIFIER);
   all_filter = g_strdup_printf ("(%s || %s)", col_filter, item_filter);
 
-  search_type = photos_search_type_new_full (PHOTOS_SEARCH_TYPE_STOCK_ALL,
-                                             _("All"),
-                                             "?urn a rdfs:Resource. "
-                                             "OPTIONAL {?item a nmm:Photo; nie:isPartOf ?urn}",
-                                             all_filter);
+  search_type = photos_search_type_new (PHOTOS_SEARCH_TYPE_STOCK_ALL,
+                                        _("All"),
+                                        "?urn a rdfs:Resource. "
+                                        "OPTIONAL {?item a nmm:Photo; nie:isPartOf ?urn}",
+                                        all_filter);
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
 
-  search_type = photos_search_type_new_full (PHOTOS_SEARCH_TYPE_STOCK_COLLECTIONS,
-                                             _("Albums"),
-                                             "?urn a nfo:DataContainer. "
-                                             "?item a nmm:Photo; nie:isPartOf ?urn.",
-                                             col_filter);
+  search_type = photos_search_type_new (PHOTOS_SEARCH_TYPE_STOCK_COLLECTIONS,
+                                        _("Albums"),
+                                        "?urn a nfo:DataContainer. "
+                                        "?item a nmm:Photo; nie:isPartOf ?urn.",
+                                        col_filter);
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
 
-  search_type = photos_search_type_new_full (PHOTOS_SEARCH_TYPE_STOCK_FAVORITES,
-                                             _("Favorites"),
-                                             "?urn a nmm:Photo; nao:hasTag nao:predefined-tag-favorite. ",
-                                             blocked_mime_types_filter);
+  search_type = photos_search_type_new (PHOTOS_SEARCH_TYPE_STOCK_FAVORITES,
+                                        _("Favorites"),
+                                        "?urn a nmm:Photo; nao:hasTag nao:predefined-tag-favorite. ",
+                                        blocked_mime_types_filter);
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
 
-  search_type = photos_search_type_new_full (PHOTOS_SEARCH_TYPE_STOCK_PHOTOS,
-                                             _("Photos"),
-                                             "?urn a nmm:Photo",
-                                             blocked_mime_types_filter);
+  search_type = photos_search_type_new (PHOTOS_SEARCH_TYPE_STOCK_PHOTOS,
+                                        _("Photos"),
+                                        "?urn a nmm:Photo",
+                                        blocked_mime_types_filter);
   photos_base_manager_add_object (PHOTOS_BASE_MANAGER (self), G_OBJECT (search_type));
   g_object_unref (search_type);
 

src/photos-search-type.c
@@ -210,14 +210,7 @@ photos_search_type_filterable_iface_init (PhotosFilterableInterface *iface)
 
 
 PhotosSearchType *
-photos_search_type_new (const gchar *id, const gchar *name)
-{
-  return g_object_new (PHOTOS_TYPE_SEARCH_TYPE, "id", id, "name", name, NULL);
-}
-
-
-PhotosSearchType *
-photos_search_type_new_full (const gchar *id, const gchar *name, const gchar *where, const gchar *filter)
+photos_search_type_new (const gchar *id, const gchar *name, const gchar *where, const gchar *filter)
 {
   return g_object_new (PHOTOS_TYPE_SEARCH_TYPE,
                        "id", id,

src/photos-search-type.h
@@ -35,9 +35,7 @@ G_DECLARE_FINAL_TYPE (PhotosSearchType, photos_search_type, PHOTOS, SEARCH_TYPE,
 #define PHOTOS_SEARCH_TYPE_STOCK_FAVORITES "favorites"
 #define PHOTOS_SEARCH_TYPE_STOCK_PHOTOS "photos"
 
-PhotosSearchType    *photos_search_type_new                (const gchar *id, const gchar *name);
-
-PhotosSearchType    *photos_search_type_new_full           (const gchar *id,
+PhotosSearchType    *photos_search_type_new                (const gchar *id,
                                                             const gchar *name,
                                                             const gchar *where,
                                                             const gchar *filter);