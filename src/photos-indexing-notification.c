GNOME/gnome-photos

commit bd087d67f938c29776842afbc3b7bc745d2ca9a2

Port to Tracker 3
Notable changes:

  * User data (favourites, albums) is now stored in a private database
    in ~/.local/share/gnome-photos. This is combined with the
    filesystem miner's index of items at query time.

  * Inside Flatpak, the application connects to Tracker via the new
    xdg-tracker-portal instead of talking directly over D-Bus. Access
    is limited by the portal so the app can only see the Pictures
    graph.

  * The Flatpak build can use a private instance of Tracker 3's
    filesystem miner when running on hosts without Tracker 3.

Currently, nfo:fileName is absent from the tracker:Pictures graph [1],
which leads to the following WARNING:
  gnome-photos-WARNING **: (../src/photos-local-item.c:91):
    photos_local_item_create_filename_fallback: code should not be
    reached

Similarly, the built-in nfo:image-category-screenshot collection is
also missing.

[1] https://gitlab.gnome.org/GNOME/tracker/-/issues/221

https://gitlab.gnome.org/GNOME/gnome-photos/-/issues/152

src/photos-indexing-notification.c
@@ -26,22 +26,23 @@
 #include <gio/gio.h>
 #include <glib.h>
 #include <glib/gi18n.h>
-#include <libtracker-control/tracker-control.h>
 
 #include "photos-application.h"
 #include "photos-gom-miner.h"
 #include "photos-indexing-notification.h"
 #include "photos-notification-manager.h"
+#include "photos-tracker-miner.h"
 
 
 struct _PhotosIndexingNotification
 {
   GtkGrid parent_instance;
+  GCancellable *cancellable;
   GtkWidget *ntfctn_mngr;
   GtkWidget *primary_label;
   GtkWidget *secondary_label;
   GtkWidget *spinner;
-  TrackerMinerManager *manager;
+  TrackerMiner *miner_files;
   gboolean closed;
   gboolean on_display;
   guint timeout_id;
@@ -55,8 +56,6 @@ enum
 {
   REMOTE_MINER_TIMEOUT = 10 /* s */
 };
-
-static const gchar *MINER_FILES = "org.gnome.Photos.Tracker1.Miner.Files";
 
 
 static void
@@ -173,23 +172,36 @@ photos_indexing_notification_timeout (gpointer user_data)
 
 
 static void
-photos_indexing_notification_check_notification (PhotosIndexingNotification *self)
+photos_indexing_notification_check_notification_get_progress (GObject *source_object,
+                                                              GAsyncResult *res,
+                                                              gpointer user_data)
 {
+  PhotosIndexingNotification *self;
   GApplication *app;
   GList *miners_running;
   GSList *running = NULL;
+  TrackerMiner *miner_files = TRACKER_MINER (source_object);
   gboolean is_indexing_local = FALSE;
   gboolean is_indexing_remote = FALSE;
+  gdouble progress;
 
-  running = tracker_miner_manager_get_running (self->manager);
-  if (g_slist_find_custom (running, (gconstpointer) MINER_FILES, (GCompareFunc) g_strcmp0) != NULL)
-    {
-      gdouble progress;
+  {
+    g_autoptr (GError) error = NULL;
 
-      tracker_miner_manager_get_status (self->manager, MINER_FILES, NULL, &progress, NULL);
-      if (progress < 1)
-        is_indexing_local = TRUE;
-    }
+    if (!tracker_miner_call_get_progress_finish (miner_files, &progress, res, &error))
+      {
+        if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+          goto out;
+
+        g_warning ("Unable to get indexing progress from TrackerMiner proxy: %s", error->message);
+        progress = 1.0;
+      }
+  }
+
+  self = PHOTOS_INDEXING_NOTIFICATION (user_data);
+
+  if (progress < 1)
+    is_indexing_local = TRUE;
 
   app = g_application_get_default ();
   miners_running = photos_application_get_miners_running (PHOTOS_APPLICATION (app));
@@ -210,7 +222,50 @@ photos_indexing_notification_check_notification (PhotosIndexingNotification *sel
   else
     photos_indexing_notification_destroy (self, FALSE);
 
+ out:
   g_slist_free_full (running, g_free);
+}
+
+
+static void
+photos_indexing_notification_check_notification (PhotosIndexingNotification *self)
+{
+  tracker_miner_call_get_progress (self->miner_files,
+                                   self->cancellable,
+                                   photos_indexing_notification_check_notification_get_progress,
+                                   self);
+}
+
+
+static void
+photos_indexing_notification_tracker_miner (GObject *source_object, GAsyncResult *res, gpointer user_data)
+{
+  PhotosIndexingNotification *self;
+  g_autoptr (TrackerMiner) miner_files = NULL;
+
+  {
+    g_autoptr (GError) error = NULL;
+
+    miner_files = tracker_miner_proxy_new_for_bus_finish (res, &error);
+    if (error != NULL)
+      {
+        if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+          g_warning ("Unable to create TrackerMiner proxy: %s", error->message);
+
+        goto out;
+      }
+  }
+
+  self = PHOTOS_INDEXING_NOTIFICATION (user_data);
+
+  self->miner_files = g_object_ref (miner_files);
+  g_signal_connect_swapped (self->miner_files,
+                            "progress",
+                            G_CALLBACK (photos_indexing_notification_check_notification),
+                            self);
+
+ out:
+  return;
 }
 
 
@@ -221,8 +276,14 @@ photos_indexing_notification_dispose (GObject *object)
 
   photos_indexing_notification_remove_timeout (self);
 
+  if (self->cancellable != NULL)
+    {
+       g_cancellable_cancel (self->cancellable);
+       g_clear_object (&self->cancellable);
+    }
+
   g_clear_object (&self->ntfctn_mngr);
-  g_clear_object (&self->manager);
+  g_clear_object (&self->miner_files);
 
   G_OBJECT_CLASS (photos_indexing_notification_parent_class)->dispose (object);
 }
@@ -236,21 +297,10 @@ photos_indexing_notification_init (PhotosIndexingNotification *self)
   GtkWidget *close;
   GtkWidget *image;
   GtkWidget *labels;
+  const gchar *miner_files_name;
 
   app = g_application_get_default ();
-
-  {
-    g_autoptr (GError) error = NULL;
-
-    self->manager = tracker_miner_manager_new_full (TRUE, &error);
-    if (error != NULL)
-      {
-        g_warning ("Unable to create a TrackerMinerManager, indexing progress notification won't work: %s",
-                   error->message);
-        return;
-      }
-  }
-
+  self->cancellable = g_cancellable_new ();
   self->ntfctn_mngr = photos_notification_manager_dup_singleton ();
 
   self->spinner = gtk_spinner_new ();
@@ -287,16 +337,21 @@ photos_indexing_notification_init (PhotosIndexingNotification *self)
   gtk_container_add (GTK_CONTAINER (self), close);
   g_signal_connect_swapped (close, "clicked", G_CALLBACK (photos_indexing_notification_close_clicked), self);
 
+  /* TODO: should be proxied by the "control" daemon for Flatpaks */
+  miner_files_name = photos_application_get_miner_files_name (PHOTOS_APPLICATION (app));
+  tracker_miner_proxy_new_for_bus (G_BUS_TYPE_SESSION,
+                                   G_DBUS_PROXY_FLAGS_NONE,
+                                   miner_files_name,
+                                   "/org/freedesktop/Tracker3/Miner/Files",
+                                   self->cancellable,
+                                   photos_indexing_notification_tracker_miner,
+                                   self);
+
   g_signal_connect_object (app,
                            "miners-changed",
                            G_CALLBACK (photos_indexing_notification_check_notification),
                            self,
                            G_CONNECT_SWAPPED);
-
-  g_signal_connect_swapped (self->manager,
-                            "miner-progress",
-                            G_CALLBACK (photos_indexing_notification_check_notification),
-                            self);
 }
 
 

data/meson.build
@@ -56,13 +56,12 @@ configure_file(
   output: domain_ontology_rule,
   configuration: tracker_domain_conf,
   install: true,
-  install_dir: join_paths(photos_datadir, 'tracker', 'domain-ontologies'),
+  install_dir: join_paths(photos_datadir, 'tracker3-miners', 'domain-ontologies'),
 )
 
 tracker_domain_services = [
-  'Tracker1',
-  'Tracker1.Miner.Extract',
-  'Tracker1.Miner.Files'
+  'Tracker3.Miner.Extract',
+  'Tracker3.Miner.Files'
 ]
 
 foreach service: tracker_domain_services

data/org.gnome.Photos.Tracker1.Miner.Extract.service.in
@@ -1,8 +1,0 @@
-[D-BUS Service]
-Name=@application_id@.Tracker1.Miner.Extract
-Exec=@libexecdir@/tracker-extract --domain-ontology @application_id@
-
-# Miner details needed for tracker-control
-Path=/org/freedesktop/Tracker1/Miner/Extract
-NameSuffix=Miner.Extract
-DisplayName=Extractor

data/org.gnome.Photos.Tracker1.Miner.Files.service.in
@@ -1,8 +1,0 @@
-[D-BUS Service]
-Name=@application_id@.Tracker1.Miner.Files
-Exec=@libexecdir@/tracker-miner-fs --initial-sleep=0 --domain-ontology @application_id@
-
-# Miner details needed for tracker-control
-Path=/org/freedesktop/Tracker1/Miner/Files
-NameSuffix=Miner.Files
-DisplayName=File System

data/org.gnome.Photos.Tracker1.service.in
@@ -1,3 +1,0 @@
-[D-BUS Service]
-Name=org.gnome.Photos.Tracker1
-Exec=@libexecdir@/tracker-store --domain-ontology org.gnome.Photos

data/org.gnome.Photos.Tracker3.Miner.Extract.service.in
@@ -1,0 +1,3 @@
+[D-BUS Service]
+Name=@application_id@.Tracker3.Miner.Extract
+Exec=@libexecdir@/tracker-extract-3 --domain-ontology @application_id@

data/org.gnome.Photos.Tracker3.Miner.Files.service.in
@@ -1,0 +1,3 @@
+[D-BUS Service]
+Name=@application_id@.Tracker3.Miner.Files
+Exec=@libexecdir@/tracker-miner-fs-3 --initial-sleep=0 --domain-ontology @application_id@

data/org.gnome.Photos.rule.in
@@ -1,3 +1,7 @@
+# This is used to run the Tracker indexer inside a Flatpak sandbox, when
+# Photos is running on a host that doesn't have a suitable version of Tracker
+# installed.
+
 [DomainOntology]
 # All locations accept
 # 1) Paths relative from a variable, the variables
@@ -14,14 +18,10 @@
 #
 
 # Location for the Tracker database (not optional)
-CacheLocation=$XDG_CACHE_HOME/gnome-photos/tracker
-
-# Location for the data journal, used in data recovery
-# situations (optional)
-JournalLocation=$XDG_DATA_HOME/gnome-photos/tracker/data
+CacheLocation=$XDG_CACHE_HOME/gnome-photos/tracker3/files
 
 # Name of the ontology to use, must be one located in
-# $(sharedir)/tracker/ontologies
+# $(sharedir)/tracker3/ontologies
 OntologyName=nepomuk
 
 # DBus name for the owner (not optional). Tracker will use

commit fab2cc25fe5e62235f22e2600b2afae5973f9354

indexing-notification: Remove unused variable
Fallout from bd087d67f938c29776842afbc3b7bc745d2ca9a2

https://gitlab.gnome.org/GNOME/gnome-photos/-/merge_requests/189

src/photos-indexing-notification.c
@@ -179,7 +179,6 @@ photos_indexing_notification_check_notification_get_progress (GObject *source_ob
   PhotosIndexingNotification *self;
   GApplication *app;
   GList *miners_running;
-  GSList *running = NULL;
   TrackerMiner *miner_files = TRACKER_MINER (source_object);
   gboolean is_indexing_local = FALSE;
   gboolean is_indexing_remote = FALSE;
@@ -223,7 +222,7 @@ photos_indexing_notification_check_notification_get_progress (GObject *source_ob
     photos_indexing_notification_destroy (self, FALSE);
 
  out:
-  g_slist_free_full (running, g_free);
+  return;
 }
 
 