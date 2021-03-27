GNOME/gnome-photos

commit aa69e5035a27c17cecc968889c0984480e2cb218

Make photos_dlna_renderer_unshare_all return a value
The convention for GIO-style asynchronous APIs is to return a value
that indicates success or failure, and not just the GError.

https://gitlab.gnome.org/GNOME/gnome-photos/-/merge_requests/197

src/photos-dlna-renderer.c
@@ -660,14 +660,14 @@ photos_dlna_renderer_unshare_all (PhotosDlnaRenderer  *self,
 }
 
 
-void
+gboolean
 photos_dlna_renderer_unshare_all_finish (PhotosDlnaRenderer *self,
                                          GAsyncResult *res,
                                          GError **error)
 {
   g_return_if_fail (g_task_is_valid (res, self));
 
-  g_task_propagate_boolean (G_TASK (res), error);
+  return g_task_propagate_boolean (G_TASK (res), error);
 }
 
 

src/photos-dlna-renderer.h
@@ -68,7 +68,7 @@ void                  photos_dlna_renderer_unshare_all        (PhotosDlnaRendere
                                                                GAsyncReadyCallback  callback,
                                                                gpointer             user_data);
 
-void                  photos_dlna_renderer_unshare_all_finish (PhotosDlnaRenderer  *self,
+gboolean              photos_dlna_renderer_unshare_all_finish (PhotosDlnaRenderer  *self,
                                                                GAsyncResult        *res,
                                                                GError             **error);
 

src/photos-remote-display-manager.c
@@ -203,8 +203,7 @@ photos_remote_display_manager_unshare_all_cb (GObject *source_object, GAsyncResu
   {
     g_autoptr (GError) error = NULL;
 
-    photos_dlna_renderer_unshare_all_finish (renderer, res, &error);
-    if (error != NULL)
+    if (!photos_dlna_renderer_unshare_all_finish (renderer, res, &error))
       g_warning ("Error while unsharing: %s", error->message);
   }
 

commit ee4ac2a644c57e609e608c7b49b1ad9c7a55bab5

dlna-renderer: Silence -Wreturn-type
Fallout from aa69e5035a27c17cecc968889c0984480e2cb218

https://gitlab.gnome.org/GNOME/gnome-photos/-/merge_requests/199

src/photos-dlna-renderer.c
@@ -668,7 +668,7 @@ photos_dlna_renderer_unshare_all_finish (PhotosDlnaRenderer *self,
                                          GAsyncResult *res,
                                          GError **error)
 {
-  g_return_if_fail (g_task_is_valid (res, self));
+  g_return_val_if_fail (g_task_is_valid (res, self), FALSE);
 
   return g_task_propagate_boolean (G_TASK (res), error);
 }